06f2b1e25399488003678a261d9a6effae91fc58
Daniel Wood <Dzwood327@gmail.com>
2024-10-10 14:49:28 -0400

    Added initial navbar component
    Still need to figure out how render the dropdown menu behind the navbar to accomplish the bg color in Figma. May need to render a separate component outside of Radix UI...

---
+++ app/components/navbar/navbar.styles.ts
@@ -0,0 +1,59 @@
+import { cva } from "class-variance-authority";
+
+export const navbarKeyframes = {
+  enterFromRight: {
+    from: { opacity: "0", transform: "translateX(200px)" },
+    to: { opacity: "1", transform: "translateX(0)" },
+  },
+  enterFromLeft: {
+    from: { opacity: "0", transform: "translateX(-200px)" },
+    to: { opacity: "1", transform: "translateX(0)" },
+  },
+  exitToRight: {
+    from: { opacity: "1", transform: "translateX(0)" },
+    to: { opacity: "0", transform: "translateX(200px)" },
+  },
+  exitToLeft: {
+    from: { opacity: "1", transform: "translateX(0)" },
+    to: { opacity: "0", transform: "translateX(-200px)" },
+  },
+  scaleIn: {
+    from: { opacity: "0", transform: "rotateX(-10deg) scale(0.9)" },
+    to: { opacity: "1", transform: "rotateX(0deg) scale(1)" },
+  },
+  scaleOut: {
+    from: { opacity: "1", transform: "rotateX(0deg) scale(1)" },
+    to: { opacity: "0", transform: "rotateX(-10deg) scale(0.95)" },
+  },
+  fadeIn: {
+    from: { opacity: "0" },
+    to: { opacity: "1" },
+  },
+  fadeOut: {
+    from: { opacity: "1" },
+    to: { opacity: "0" },
+  },
+};
+
+export const navbarAnimations = {
+  scaleIn: "scaleIn 200ms ease",
+  scaleOut: "scaleOut 200ms ease",
+  fadeIn: "fadeIn 200ms ease",
+  fadeOut: "fadeOut 200ms ease",
+  enterFromLeft: "enterFromLeft 250ms ease",
+  enterFromRight: "enterFromRight 250ms ease",
+  exitToLeft: "exitToLeft 250ms ease",
+  exitToRight: "exitToRight 250ms ease",
+};
+
+export const navigationMenuTriggerStyle = cva(
+  "group inline-flex h-10 w-max items-center justify-center text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=open]:border-b-3 border-primary text-base font-bold"
+);
+
+export const navigationMenuContentStyle = cva(
+  "fixed top-[72px] left-0 data-[motion=from-end]:animate-enterFromRight data-[motion=from-start]:animate-enterFromLeft data-[motion=to-end]:animate-exitToRight data-[motion=to-start]:animate-exitToLeft sm:w-auto"
+);
+
+export const angleDownIconStyle = cva(
+  "relative top-[1px] ml-1 size-6 transition duration-200 group-data-[state=open]:rotate-180"
+);
