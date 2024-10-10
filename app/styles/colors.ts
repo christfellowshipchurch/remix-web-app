b85568ec64af97c1bad2019980b5d021654051dc
Daniel Wood <Dzwood327@gmail.com>
2024-10-10 14:49:28 -0400

    Added initial navbar component
    Still need to figure out how render the dropdown menu behind the navbar to accomplish the bg color in Figma. May need to render a separate component outside of Radix UI...

--- app/styles/colors.ts
+++ app/styles/colors.ts
@@ -1,4 +1,4 @@
-import Color from "color"
+import Color from "color";
 
 function createPallette(color: string) {
   return {
@@ -12,49 +12,53 @@ function createPallette(color: string) {
     800: Color(color).darken(0.5).hex(),
     900: Color(color).darken(0.7).hex(),
     DEFAULT: color,
-  }
+  };
 }
 
 //Brand Colors
-const OCEAN = "#0092bc"
-const OCEAN_SUBDUED = "#E7F9FE"
-const NAVY = "#004f71"
-const NAVY_SUBDUED = "#DAEAF1"
-const COTTON_CANDY = "#6bcaba"
+const OCEAN = "#0092bc";
+const OCEAN_SUBDUED = "#E7F9FE";
+const NAVY = "#004f71";
+const NAVY_SUBDUED = "#DAEAF1";
+const COTTON_CANDY = "#6bcaba";
 
 //Accent Colors
 //const COCONUT = "#d0d0ce"
-const APPLE = "#cb2c30"
-const TANGERINE = "#ff8f1c"
-const PEACH = "#e8927c"
-const LEMON = "#fcd757"
+const APPLE = "#cb2c30";
+const TANGERINE = "#ff8f1c";
+const PEACH = "#e8927c";
+const LEMON = "#fcd757";
 
 //System Colors
-const SUCCESS = "#1EC27F"
+const SUCCESS = "#1EC27F";
 //const SUCCESS_LIGHT = "#ecfdf3"
-const ERROR = "#b42318"
+const ERROR = "#b42318";
 //const ERROR_LIGHT = "#fef3f2"
-const WARNING = "#E09541"
+const WARNING = "#E09541";
 
 //Neutral Colors
-const WHITE = "#ffffff"
-const BLACK = "#000000"
-const WHITE_SMOKE = "#f6f6f6"
+const WHITE = "#ffffff";
+const BLACK = "#000000";
+const WHITE_SMOKE = "#f6f6f6";
 
 //Text Colors
-const TEXT_PRIMARY = "#222222"
-const TEXT_SECONDARY = "#666666"
+const TEXT_PRIMARY = "#222222";
+const TEXT_SECONDARY = "#666666";
+
+//Background Colors
+const BACKGROUND_PRIMARY = "#FFFFFF";
+const BG_SECONDARY = "#FAFAFC";
 
 //Link Colors
-const LINK_SECONDARY = "#666666"
+const LINK_SECONDARY = "#666666";
 
 //Campaign Colors
-const H4H_RED_2024 = "#E63E51"
+const H4H_RED_2024 = "#E63E51";
 
 // Article Colors
-const NEWSLETTER_FROM = "#1C3647"
-const NEWSLETTER_TO = "#004f71"
-const BACKGROUND_TO = "#EEEEEE"
+const NEWSLETTER_FROM = "#1C3647";
+const NEWSLETTER_TO = "#004f71";
+const BACKGROUND_TO = "#EEEEEE";
 
 const colors = {
   primary: createPallette(OCEAN),
@@ -100,6 +104,9 @@ const colors = {
   text_primary: TEXT_PRIMARY,
   text_secondary: TEXT_SECONDARY,
 
+  background_primary: BACKGROUND_PRIMARY,
+  background_secondary: BG_SECONDARY,
+
   link_secondary: LINK_SECONDARY,
 
   newsletter_from: NEWSLETTER_FROM,
@@ -109,6 +116,6 @@ const colors = {
   h4h: {
     red: H4H_RED_2024,
   },
-}
+};
 
-export default colors
+export default colors;
