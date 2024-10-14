fe4cc45f7e84ecbeaae705e150a07dbde64fab24
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-10-14 13:19:36 -0400

    location search vid fix

--- app/routes/locations/partials/locationsSearch.partial.tsx
+++ app/routes/locations/partials/locationsSearch.partial.tsx
@@ -22,7 +22,7 @@ export const Search = ({
 }: SearchProps) => {
   return (
     <div className="flex h-[70vh] w-full items-center justify-center md:h-[82vh]">
-      <div className="relative flex size-full">
+      <div className="relative flex size-full overflow-hidden">
         <Video
           wistiaId="padj4c4xoh"
           autoPlay
