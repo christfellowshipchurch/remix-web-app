46cdee3e8d1eed045a6f06ccddd2b670f694453c
Daniel Wood <Dzwood327@gmail.com>
2024-10-16 11:18:49 -0400

    Added Article Error Boundary

--- app/routes/articles_.$path.tsx
+++ app/routes/articles_.$path.tsx
@@ -4,4 +4,7 @@ export { loader } from "./articles/loader";
 export { action } from "./articles/action";
 export { meta } from "./articles/meta";
 
+// Using root ErrorBoundary
+export { ErrorBoundary } from "../error";
+
 export default ArticlePage;
