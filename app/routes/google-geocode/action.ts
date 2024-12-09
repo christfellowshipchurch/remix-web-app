072284e85271c060c62bcc3549620cff93a8e91b
Daniel Wood <Dzwood327@gmail.com>
2024-12-09 17:03:06 -0500

    Started the error handling for invalid zipcodes

--- app/routes/google-geocode/action.ts
+++ app/routes/google-geocode/action.ts
@@ -1,15 +1,40 @@
 import { ActionFunction, json } from "@remix-run/node";
 import { LocationSearchCoordinatesType } from "../locations/locationSearch/location-search";
+import {
+  AuthenticationError,
+  EncryptionError,
+  RockAPIError,
+} from "~/lib/.server/errorTypes";
 
 export const action: ActionFunction = async ({ request }) => {
-  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-  const formData = Object.fromEntries(await request.formData());
-  const address = formData.address as string;
+  try {
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+    const formData = Object.fromEntries(await request.formData());
+    const address = formData.address as string;
 
-  const response = await fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`
-  );
-  const data: LocationSearchCoordinatesType = (await response?.json()) as any;
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`
+    );
 
-  return json(data);
+    if (!response.ok) {
+      throw new Error("Failed to fetch geocode data");
+    }
+
+    const data: LocationSearchCoordinatesType = (await response.json()) as any;
+
+    if (data.status === "ZERO_RESULTS") {
+      throw new Error("Failed to fetch geocode data");
+    }
+
+    return json(data);
+  } catch (error) {
+    if (
+      error instanceof AuthenticationError ||
+      error instanceof EncryptionError ||
+      error instanceof RockAPIError
+    ) {
+      return json({ error: error.message }, { status: 400 });
+    }
+    return json({ error: "An unexpected error occurred" }, { status: 500 });
+  }
 };
--- app/routes/locations/locationSearch/location-search.tsx
+++ app/routes/locations/locationSearch/location-search.tsx
@@ -16,6 +16,7 @@ export type LocationSearchCoordinatesType = {
       };
     }
   ];
+  status: string;
 };
 
 export function LocationSearchPage() {
