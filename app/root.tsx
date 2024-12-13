3d8af2eeff9634d548843b8aa5bdcb364cb9e39b
Daniel Wood <Dzwood327@gmail.com>
2024-12-13 09:34:15 -0500

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,5 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 
-// import "./styles/tailwind.css";
 import Navbar from "./components/navbar";
 import { AuthProvider } from "./providers/auth-provider";
 
@@ -10,6 +9,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* Moved tailwind import here to support SSR better */}
         <link rel="stylesheet" href="../app/styles/tailwind.css" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
