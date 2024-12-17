90ee5084032cd6911ec0d2bdd6a893b58ec33e19
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-12-17 17:00:03 -0500

    groups

---
+++ app/routes/group-finder/types.ts
@@ -0,0 +1,62 @@
+export interface GroupHit {
+  rockItemId: number;
+  title: string;
+  summary: string;
+  leaders: {
+    firstName: string;
+    lastName: string;
+    photo: {
+      uri: string;
+    };
+  }[];
+  priority: number;
+  action: string;
+  campusName: string;
+  meetingDay: string;
+  meetingType: string;
+  preferences: string[];
+  coverImage: {
+    sources: {
+      uri: string;
+    }[];
+  };
+  _typename: string;
+  objectID: string;
+  _highlightResult: {
+    title: {
+      value: string;
+      matchLevel: string;
+      matchedWords: string[];
+    };
+    summary: {
+      value: string;
+      matchLevel: string;
+      matchedWords: string[];
+    };
+    author: {
+      firstName: {
+        value: string;
+        matchLevel: string;
+        matchedWords: string[];
+      };
+      lastName: {
+        value: string;
+        matchLevel: string;
+        matchedWords: string[];
+      };
+    };
+    routing: {
+      pathname: {
+        value: string;
+        matchLevel: string;
+        matchedWords: string[];
+      };
+    };
+    htmlContent: {
+      value: string;
+      matchLevel: string;
+      matchedWords: string[];
+    }[];
+  };
+  __position: number;
+}
