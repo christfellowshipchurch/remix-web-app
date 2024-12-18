aeccf2c1be293ac6f5cb3efd50975b5fc35aa5a3
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-12-18 16:59:31 -0500

    cards fix

--- app/routes/group-finder/group-finder.tsx
+++ app/routes/group-finder/group-finder.tsx
@@ -4,7 +4,13 @@ import { GroupSearch } from "./partials/GroupSearch.partial";
 export function GroupFinder() {
   return (
     <div className="flex flex-col items-center">
-      <DynamicHero imagePath="../app/assets/images/groups-hero-bg.jpg" />
+      <DynamicHero
+        imagePath="../app/assets/images/groups-hero-bg.jpg"
+        ctas={[
+          { title: "Lead a Group", href: "/lead-a-group" },
+          { title: "Help me find a Group", href: "#search" },
+        ]}
+      />
       <GroupSearch />
     </div>
   );
