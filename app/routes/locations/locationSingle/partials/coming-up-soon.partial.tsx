19ac3c8796b31d0f14db95ab29c5754033d53bb5
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-01-14 15:38:47 -0500

    Coming Up fix

--- app/routes/locations/locationSingle/partials/coming-up-soon.partial.tsx
+++ app/routes/locations/locationSingle/partials/coming-up-soon.partial.tsx
@@ -14,31 +14,30 @@ export const ComingUpSoon = () => {
 
       <div className="flex gap-4 overflow-x-auto max-w-full lg:justify-center lg:items-center lg:w-full lg:overflow-visible lg:max-w-[1440px]">
         {comingUpSoon?.cards?.map((card, index) => (
-          <div key={index}></div>
-          // <Link
-          //   to={card.url}
-          //   prefetch="intent"
-          //   key={index}
-          //   style={{
-          //     boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.25)",
-          //     marginLeft: index === 0 ? 16 : 0,
-          //     marginRight: index === comingUpSoon.cards.length - 1 ? 16 : 0,
-          //     ...heroBgImgStyles(card.image),
-          //   }}
-          //   className="w-[90vw] lg:w-[370px] h-[450px] rounded-md flex items-center relative transition-transform duration-300 hover:-translate-y-3 shrink-0"
-          // >
-          //   <div
-          //     className="absolute size-full opacity-80"
-          //     style={{
-          //       background:
-          //         "linear-gradient(to bottom, rgba(0, 0, 0, 0), #353535)",
-          //     }}
-          //   />
-          //   <div className="absolute self-end text-white p-5">
-          //     <h2 className="font-bold text-xl">{card.title}</h2>
-          //     <p>{card.description}</p>
-          //   </div>
-          // </Link>
+          <Link
+            to={card.url}
+            prefetch="intent"
+            key={index}
+            style={{
+              boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.25)",
+              marginLeft: index === 0 ? 16 : 0,
+              marginRight: index === comingUpSoon.cards.length - 1 ? 16 : 0,
+              ...heroBgImgStyles(card.image),
+            }}
+            className="w-[90vw] lg:w-[370px] h-[450px] rounded-md flex items-center relative transition-transform duration-300 hover:-translate-y-3 shrink-0"
+          >
+            <div
+              className="absolute size-full opacity-80"
+              style={{
+                background:
+                  "linear-gradient(to bottom, rgba(0, 0, 0, 0), #353535)",
+              }}
+            />
+            <div className="absolute self-end text-white p-5">
+              <h2 className="font-bold text-xl">{card.title}</h2>
+              <p>{card.description}</p>
+            </div>
+          </Link>
         ))}
       </div>
 
