cbb530c91f328b2ef8b1065e633255e613000e5a
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-01-16 15:03:45 -0500

    Fixed max width of messages

--- app/routes/messages/message-single/partials/series.partial.tsx
+++ app/routes/messages/message-single/partials/series.partial.tsx
@@ -7,14 +7,14 @@ export const InThisSeries = () => {
   const { message } = useLoaderData<MessageReturnType>();
   // Get Series ID and get episodes in this series
   return (
-    <div className="flex w-full flex-col gap-6 py-12 md:py-24 lg:max-w-xl xl:max-w-xxl">
+    <div className="flex w-full flex-col gap-6 py-12 md:py-24 lg:max-w-5xl xl:max-w-6xl">
       <div className="flex flex-col gap-2">
         <h2 className="heading-h3">In This Series</h2>
         {/* Series Description */}
         <p className="text-[#AAAAAA]">How to Master Your Money</p>
       </div>
       {/* Other Series Messages */}
-      <div className="grid grid-cols-4 gap-6 xl:gap-8 pb-2">
+      <div className="flex overflow-x-auto gap-6 xl:gap-8 pb-2">
         {mockInThisSeries.map((message, i) => (
           <SeriesCard key={i} data={message} />
         ))}
