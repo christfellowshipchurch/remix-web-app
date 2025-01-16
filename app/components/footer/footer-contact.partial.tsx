89518fb282ef49ef07f9c1140e1b7536d0d7942d
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-01-16 15:52:26 -0500

    start

--- app/components/footer/footer-contact.partial.tsx
+++ app/components/footer/footer-contact.partial.tsx
@@ -7,14 +7,14 @@ const SideDivider = () => (
 
 export const ContactInfo = () => {
   return (
-    <div className="flex flex-col items-center lg:flex-row py-12 lg:gap-6 xl:gap-24">
+    <div className="flex flex-col items-center lg:flex-row py-12 lg:gap-6 xl:gap-10">
       {/* Call */}
-      <div className="flex justify-center items-center w-full pb-6 lg:pb-0 gap-4 lg:justify-start">
+      <div className="flex items-center w-full pb-6 lg:pb-0 gap-4 lg:justify-start">
         <div className="min-w-12">
           <Icon name="call" size={50} color="#D0D0CE" />
         </div>
         <div className="flex flex-col h-full justify-center">
-          <h2 className="text-lg text-[#D0D0CE]">Call Us</h2>
+          <h2 className="text-lg font-medium text-[#D0D0CE]">Call Us</h2>
           <a className={linkStyle} href="tel:5617997600">
             (561) 799-7600
           </a>
@@ -22,12 +22,12 @@ export const ContactInfo = () => {
       </div>
       <SideDivider />
       {/* Email */}
-      <div className="flex justify-center items-center w-full gap-4 border-y py-6 lg:py-0 lg:border-y-0 border-[#D9D9D9]/30 lg:justify-start">
+      <div className="flex items-center w-full gap-4 border-y py-6 lg:py-0 lg:border-y-0 border-[#D9D9D9]/30 lg:justify-start">
         <div className="min-w-12">
           <Icon name="envelope" size={50} color="#D0D0CE" />
         </div>
         <div className="flex flex-col h-full justify-center">
-          <h2 className="text-lg text-[#D0D0CE]">Need Support</h2>
+          <h2 className="text-lg font-medium text-[#D0D0CE]">Email Us</h2>
           <a
             href="mailto:hello@christfellowship.church"
             className={`${linkStyle} w-full max-w-[324px]`}
@@ -38,12 +38,12 @@ export const ContactInfo = () => {
       </div>
       <SideDivider />
       {/* Location */}
-      <div className="flex justify-center items-center w-full gap-4 pt-6 lg:pt-0 lg:justify-start lg:w-full lg:min-w-[408px]">
+      <div className="flex items-center w-full gap-4 pt-6 lg:pt-0 lg:justify-start lg:w-full lg:min-w-[408px]">
         <div className="min-w-12">
-          <Icon name="world" size={50} color="#D0D0CE" />
+          <Icon name="map" size={50} color="#D0D0CE" />
         </div>
         <div className="flex flex-col h-full justify-center">
-          <h2 className="text-lg text-[#D0D0CE]">Main Campus Office</h2>
+          <h2 className="text-lg font-medium  text-[#D0D0CE]">Visit Us</h2>
           <a
             target="_blank"
             href="https://goo.gl/maps/Uv6zB3aFzr8pkfzz9"
