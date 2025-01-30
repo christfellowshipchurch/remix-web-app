6b255af6c735115ea17f582e50790053bd349305
Daniel Wood <Dzwood327@gmail.com>
2025-01-30 16:00:24 -0500

    Updated All Events
    - removed Hero section
    - fixed card alignment
    - updated navbar to light mode since no more Hero section

--- app/routes/events/all-events/partials/featured-events.partial.tsx
+++ app/routes/events/all-events/partials/featured-events.partial.tsx
@@ -12,7 +12,7 @@ export function FeaturedEvents() {
     <div className="flex flex-col">
       <SectionTitle sectionTitle="featured events." />
       <FeaturedCard card={featuredEvent} />
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 xl:gap-16 place-items-center md:place-items-start">
+      <div className="mt-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 xl:gap-16 place-items-center md:place-items-start">
         {otherEvents.map((event, i) => (
           <ContentCard
             key={i}
