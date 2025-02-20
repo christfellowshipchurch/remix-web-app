f466171af62ccde44433afd420ed444a4346faf8
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2025-02-20 14:04:43 -0500

    Connect Current Series to Rock API Data
    - Fetch latest message details from Rock API in loader
    - Update CurrentSeries component to dynamically render latest message information
    - Add support for displaying series title, message details, and dynamic links
    - Implement HTMLRenderer for message description

--- app/routes/messages/all-messages/components/current-series.component.tsx
+++ app/routes/messages/all-messages/components/current-series.component.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useLoaderData, Link } from "react-router-dom";
 import SectionTitle from "~/components/section-title";
 import { Button } from "~/primitives/button/button.primitive";
 import Icon from "~/primitives/icon";
+import HTMLRenderer from "~/primitives/html-renderer";
+import type { LoaderReturnType } from "../loader";
 
 const CurrentSeries: React.FC = () => {
+  const { currentSeries } = useLoaderData<LoaderReturnType>();
+  const { currentSeriesTitle, latestMessage } = currentSeries;
+
   return (
     <section className="relative py-32 min-h-screen bg-white content-padding">
       {/* Light Blue Background - Bottom Half */}
@@ -15,7 +21,7 @@ const CurrentSeries: React.FC = () => {
         <SectionTitle
           className="mb-8"
           sectionTitle="current series."
-          title="Timeless Truths For Truthless Times"
+          title={currentSeriesTitle}
         />
 
         {/* Content Section */}
@@ -24,32 +30,45 @@ const CurrentSeries: React.FC = () => {
           <div
             className="lg:absolute lg:inset-0 w-full aspect-video lg:h-full bg-cover bg-center"
             style={{
-              backgroundImage:
-                "url('https://cloudfront.christfellowship.church/GetImage.ashx?guid=1d311eaf-39ef-40cc-ad42-3e11b89d0051')",
+              backgroundImage: `url('${latestMessage.coverImage}')`,
             }}
           />
 
           {/* Text Section */}
-          <div className="lg:absolute bottom-0 left-0 w-full bg-white px-10 py-16 space-y-4 lg:w-[45%] max-h-[500px]">
+          <div className="lg:absolute bottom-0 left-0 w-full bg-white px-10 py-16 space-y-4 lg:w-[45%] md:max-h-[500px]">
             <h3 className="text-lg font-bold text-ocean">Latest Message</h3>
-            <h2 className="text-[40px] font-bold text-text-primary">
-              What is Truth?
+            <h2 className="text-[40px] font-bold text-text-primary text-pretty">
+              {latestMessage.title}
             </h2>
-            <p className="font-bold">Pastor Ryan McDermott</p>
-            <p className="text-gray-700">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse varius enim in eros elementum tristique. Duis cursus,
-              mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam
-              libero vitae erat.
-            </p>
+            <p className="font-bold">{latestMessage.authorName}</p>
+            <div className="text-gray-700 line-clamp-4 prose prose-p:my-0">
+              <HTMLRenderer html={latestMessage.description} />
+            </div>
 
             {/* Buttons */}
-            <div className="flex flex-col md:flex-row pt-10 space-y-4 md:space-x-4">
-              <Button intent="secondary">Series & Resources</Button>
-              <Button intent="primary" className="flex items-center gap-2">
-                Watch Message
-                <Icon name="arrowRight" />
-              </Button>
+            <div className="mt-5 md:mt-0 flex flex-col md:flex-row gap-3">
+              <Link to="/messages/series">
+                <Button
+                  intent="secondary"
+                  className="font-semibold border-ocean rounded-none hover:enabled:bg-ocean/10 px-6 py-3"
+                >
+                  Series & Resources
+                </Button>
+              </Link>
+              <div className="group">
+                <Link to={latestMessage.path} className="flex">
+                  <Button
+                    intent="secondary"
+                    className="font-semibold border-ocean rounded-none hover:enabled:bg-ocean/10"
+                  >
+                    Watch Message
+                  </Button>
+                  <Icon
+                    name="arrowBack"
+                    className="ml-[-0.75rem] text-white rounded-full bg-ocean p-2 rotate-[135deg] transition-transform duration-300 group-hover:rotate-180 size-10"
+                  />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
