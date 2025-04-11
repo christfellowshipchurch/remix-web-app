c0ec889785d5d8b4bc6e0da76e3f97ee79ea5573
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-04-11 17:10:27 -0400

    Messages Single

--- app/components/video-header/index.tsx
+++ app/components/video-header/index.tsx
@@ -5,6 +5,7 @@ import { Video } from "~/primitives/video/video.primitive";
 
 import { Divider } from "~/routes/articles/all-articles/partials/latest.partial";
 import { Link } from "react-router";
+import { IconButton } from "~/primitives/button/icon-button.primitive";
 
 export type VideoHeaderTypes = {
   ctas?: { href: string; title: string }[];
@@ -20,10 +21,9 @@ export const VideoHeader = ({
   fallback,
 }: VideoHeaderTypes) => {
   // TODO: Make sure videos in Wistia have the controls removed and the theme is set to what it's in Figma...
-  // TODO: Update margin top once the navbar is updated?
   return (
     <div className="flex items-center justify-start self-stretch content-padding">
-      <div className="flex flex-col gap-12 w-full pb-16 mx-auto max-w-screen-content items-start justify-end self-stretch">
+      <div className="flex flex-col gap-0 md:gap-12 w-full pb-10 lg:pb-0 mx-auto max-w-screen-content items-start justify-end self-stretch">
         {wistiaId ? (
           <div className="w-full">
             <Video wistiaId={wistiaId} fallback={fallback} />
@@ -34,27 +34,39 @@ export const VideoHeader = ({
         <div className="flex items-center justify-between self-stretch">
           {/* Breadcrumbs */}
           <div className="flex flex-col gap-3 w-full md:px-0 md:flex-row md:items-center md:justify-between md:gap-0">
-            <Breadcrumbs />
+            <div className="hidden lg:block">
+              <Breadcrumbs mode="light" />
+            </div>
 
-            {/* CTAs */}
-            <div className="mt-5 md:mt-0 flex justify-start gap-3 pr-1 md:pr-4 md:gap-4 relative group">
-              {ctas?.map((cta, i) => (
-                <Link className="relative" key={i} to={cta.href}>
-                  <Button
-                    intent="secondary"
-                    className="font-semibold text-white border-white rounded-none hover:enabled:bg-slate-300/20 px-2 py-1.5"
+            {/* Desktop CTAs */}
+            <div className="hidden lg:block">
+              <div className="mt-0 flex flex-row justify-start gap-4 relative group">
+                {ctas?.map((cta, i) => (
+                  <IconButton
+                    key={i}
+                    to={cta.href}
+                    className="text-white border-[#FAFAFC] border w-full"
+                    withRotatingArrow={i === ctas.length - 1}
                   >
                     {cta.title}
-                  </Button>
-                  {/* TODO: incorporate this into the button component */}
-                  <div
-                    className={`${
-                      i < 1 ? "hidden" : ""
-                    } absolute rounded-full p-1 md:p-2 bg-ocean -right-5 top-[50%] translate-y-[-50%] rotate-[135deg] group-hover:rotate-180 transition-all duration-300`}
-                  >
-                    <Icon name="arrowBack" size={26} color="white" />
-                  </div>
-                </Link>
+                  </IconButton>
+                ))}
+              </div>
+            </div>
+
+            {/* Mobile CTAs */}
+            <div className="lg:hidden flex flex-col md:flex-row gap-3 w-full pt-8 md:pt-0 md:px-0">
+              {ctas?.map((cta, i) => (
+                <Button
+                  key={i}
+                  intent={i === 0 ? "primary" : "secondary"}
+                  href={cta.href}
+                  className={`w-full md:w-auto ${
+                    i !== 0 ? "text-white border-white" : ""
+                  }`}
+                >
+                  {cta.title}
+                </Button>
               ))}
             </div>
           </div>
