86cebd166c6b562681700400ba87416a15185cb8
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-04-15 15:31:08 -0400

    Groups

--- app/routes/group-finder/finder/components/hit-component.component.tsx
+++ app/routes/group-finder/finder/components/hit-component.component.tsx
@@ -14,7 +14,7 @@ export function HitComponent({ hit }: { hit: GroupHit }) {
 
   return (
     <div
-      className="h-full mb-4 bg-white rounded-lg overflow-hidden max-w-[300px] xl:max-w-96"
+      className="h-full mb-4 bg-white rounded-lg overflow-hidden max-w-[306px] xl:max-w-96"
       style={{
         boxShadow:
           "0 10px 15px -3px rgba(0, 0, 0, 0.1),0 4px 6px -2px rgba(0, 0, 0, 0.05)",
