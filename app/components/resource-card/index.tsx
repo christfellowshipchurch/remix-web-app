6f663c9425d7ea86d6148a19de8b9301640ed146
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-04-16 18:25:02 -0400

    images update

--- app/components/resource-card/index.tsx
+++ app/components/resource-card/index.tsx
@@ -8,7 +8,7 @@ export function ResourceCard({
   image: string;
 }) {
   return (
-    <div className="rounded-[8px] border border-neutral-lighter overflow-hidden">
+    <div className="rounded-[8px] border border-neutral-lighter overflow-hidden shadow-md cursor-pointer hover:translate-y-[-4px] transition-all duration-300">
       <img src={image} alt={title} className="w-full h-[278px] object-cover" />
       <div className="flex flex-col p-6 gap-4 bg-white">
         <h3 className="text-[24px] font-bold leading-none">{title}</h3>
