41a4fb770220641c5e3270388505c77876541c7c
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-04-18 13:29:10 -0400

    Fixed mobile and dots

--- app/routes/series-resources/index.tsx
+++ app/routes/series-resources/index.tsx
@@ -23,7 +23,7 @@ export function SeriesResources() {
             <p className="lg:max-w-[720px] lg:text-lg">{series.description}</p>
 
             {/* CTAs */}
-            <div className="flex flex-col md:flex-row gap-4">
+            <div className="flex flex-col md:flex-row gap-4 w-full  md:w-auto">
               {series.ctas?.map((cta, index) => (
                 <Button
                   key={index}
