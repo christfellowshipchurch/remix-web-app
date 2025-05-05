6373ef7c0f9f82980d202b048394e9504470c4d0
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-05-05 12:52:28 -0400

    Enhance Leadership and What to Expect Sections with Improved Layout and Styling
    - Updated the `LeadershipSection` to improve layout and responsiveness with flexbox adjustments.
    - Refined the `WhatToExpectDesktopTabs` component for better visual structure and user interaction.
    - Enhanced `WhatToExpectDesktopCard` styles for improved readability and alignment.
    - Adjusted content in `WhatToExpectData` for clarity and consistency in naming.
    - Improved overall spacing and padding in `WhatToExpectSection` for a more cohesive design.

--- app/routes/about/partials/leadership.partial.tsx
+++ app/routes/about/partials/leadership.partial.tsx
@@ -10,11 +10,11 @@ export function LeadershipSection({ bg }: { bg?: string }) {
       className={cn("pt-16 pb-24 lg:px-18 w-full", bg || "bg-gray")}
     >
       <div className="max-w-screen-content mx-auto ">
-        <div className="content-padding lg:px-0">
+        <div className="content-padding lg:px-0 lg:flex flex-col lg:gap-28">
           {/* Desktop title */}
-          <div className="hidden lg:block">
-            <SectionTitle className="mb-6" sectionTitle="our team." />
-            <h3 className="text-5xl font-extrabold leading-tight mb-16 max-w-3xl">
+          <div className="hidden lg:flex gap-24 items-center">
+            <SectionTitle sectionTitle="our team." />
+            <h3 className="text-5xl font-extrabold leading-none max-w-3xl">
               Meet The Passionate Leaders <br />
               Of Christ Fellowship Church.
             </h3>
