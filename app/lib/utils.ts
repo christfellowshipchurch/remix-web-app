39ec7959461598a36c00a66214475d50aea839b2
Daniel Wood <Dzwood327@gmail.com>
2025-05-06 13:18:17 -0400

    Setup Initial loader for Link Tree Pages
    TODO: figure out why we can GetChildren from the Link Treed Content Channel Items🤔

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -218,3 +218,20 @@ export const getFirstParagraph = (html: string): string => {
   const firstParagraph = doc.querySelector("p");
   return firstParagraph?.textContent || "";
 };
+
+interface CallToAction {
+  title: string;
+  url: string;
+}
+
+export const parseRockKeyValueList = (input: string): CallToAction[] => {
+  if (!input) return [];
+
+  return input.split("|").map((item) => {
+    const [title, url] = item.split("^#");
+    return {
+      title: title.trim(),
+      url: url.trim(),
+    };
+  });
+};
