bcb2a41b152d21860f89cdd3f98815be82095427
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-05-16 16:14:23 -0400

    Limiting Collection to two rows

--- app/components/page-builder/cta-collection.tsx
+++ app/components/page-builder/cta-collection.tsx
@@ -65,9 +65,9 @@ const PageBuilderCTACollection = ({
           </Button>
         </div>
 
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-8 pb-12">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-8 pb-12 grid-rows-2 overflow-hidden">
           {resources.map((resource) => (
-            <ResourceCard resource={resource} />
+            <ResourceCard key={resource.id} resource={resource} />
           ))}
         </div>
 
