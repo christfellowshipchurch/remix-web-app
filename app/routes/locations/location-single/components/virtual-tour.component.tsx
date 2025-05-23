7199a3cea69b3c9727c7f7e66b08ac52b07ae1a2
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-05-23 09:55:28 -0400

    Add Virtual Tour Component
    Introduced a new Virtual Tour component with tabbed navigation for map and video content. The component utilizes Radix UI Tabs for seamless user interaction and includes a responsive design with TailwindCSS. This enhances the user experience by allowing visitors to explore the campus virtually or in person.

---
+++ app/routes/locations/location-single/components/virtual-tour.component.tsx
@@ -0,0 +1,81 @@
+import * as Tabs from "@radix-ui/react-tabs";
+import { Icon } from "~/primitives/icon/icon";
+import { Video } from "~/primitives/video/video.primitive";
+
+export const VirtualTour = ({ wistiaId }: { wistiaId: string }) => {
+  return (
+    <div className="flex flex-col py-8 rounded-[18px] border border-neutral-lighter">
+      <Tabs.Root defaultValue="tour">
+        <TabContent
+          value="map"
+          image="/images/map.png"
+          title="Visit Us"
+          description="Come experience our campus in person! Our friendly staff is ready
+              to give you a tour and answer any questions you may have about our
+              programs and facilities."
+        />
+        <TabContent
+          value="tour"
+          title="Take a Virtual Tour"
+          description="Experience our campus before you visit. "
+          wistiaId={wistiaId}
+        />
+
+        <Tabs.List className="flex gap-4 p-8">
+          <TourButton value="map">Map</TourButton>
+          <TourButton value="tour">Virtual Tour</TourButton>
+        </Tabs.List>
+      </Tabs.Root>
+    </div>
+  );
+};
+
+const TourButton = ({
+  children,
+  value,
+}: {
+  children: React.ReactNode;
+  value: string;
+}) => {
+  return (
+    <Tabs.Trigger
+      className="flex-1 w-full max-w-[300px] py-2 flex gap-2 items-center justify-center text-sm font-semibold rounded-[200px] md:rounded-[400px] border border-[#D6D6D6] text-black"
+      value={value}
+    >
+      <Icon
+        name={value === "map" ? "mapAlt" : "video"}
+        size={20}
+        color="black"
+      />
+      {children}
+    </Tabs.Trigger>
+  );
+};
+
+const TabContent = ({
+  image,
+  title,
+  description,
+  wistiaId,
+  value,
+}: {
+  image?: string;
+  title: string;
+  description: string;
+  wistiaId?: string;
+  value: string;
+}) => {
+  return (
+    <Tabs.Content value={value}>
+      <div className="flex flex-col p-8 !pt-0">
+        <h3 className="text-lg font-bold">{title}</h3>
+        <p>{description}</p>
+      </div>
+
+      {image && <img src={image} alt={title} />}
+      {wistiaId && (
+        <Video wistiaId={wistiaId} className="aspect-video max-w-[670px]" />
+      )}
+    </Tabs.Content>
+  );
+};
