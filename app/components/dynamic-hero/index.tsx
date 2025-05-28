b3756e489e51194ebcff567f11ad3103ff359cbd
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-05-28 15:48:51 -0400

    Refactor Campus Info and Location Single Components
    - Updated `CampusInfo` to utilize a new `weekdaySchedule` structure for improved data handling.
    - Enhanced the `IconText` component for better display of service times and contact information.
    - Adjusted the `LocationSingle` component to pass the updated props to `CampusInfo`, ensuring accurate data representation.
    - Cleaned up unused props and improved layout for better responsiveness and user experience.

--- app/components/dynamic-hero/index.tsx
+++ app/components/dynamic-hero/index.tsx
@@ -72,110 +72,116 @@ export const DynamicHero = ({
       {/* Content */}
       <div
         className={cn(
-          "flex flex-col w-full mx-auto items-start justify-end self-stretch max-w-screen-content z-10",
-          "gap-8 pb-8 md:gap-12 md:pb-16",
+          "flex flex-col w-full items-start justify-end self-stretch z-10",
           "px-5 md:px-12 lg:px-18"
         )}
       >
-        <h1 className="font-extrabold heading-h1 text-[3rem] md:text-[4rem] lg:text-[100px] text-white">
-          {customTitle ? <HTMLRenderer html={customTitle} /> : pagePath}
-        </h1>
         <div
-          role="separator"
-          aria-hidden="true"
-          className="hidden md:block h-[2px] self-stretch bg-[#D9D9D9] opacity-50"
-        />
-        <div className="flex items-center justify-between self-stretch">
-          {/* Breadcrumbs */}
-          <div className="flex flex-col gap-3 w-full md:px-0 md:flex-row md:items-center md:justify-between md:gap-0">
-            <div className="hidden lg:block">
-              <Breadcrumbs mode="light" />
-            </div>
-            <div
-              role="separator"
-              aria-hidden="true"
-              className="md:hidden h-[2px] self-stretch bg-[#D9D9D9]"
-            />
-            {/* Desktop CTAs */}
-            <div className="hidden lg:flex mt-5 flex-wrap justify-between gap-3 pr-1">
-              {ctas?.map((cta, i) => {
-                if (cta.isSetAReminder) {
-                  const ReminderButton = ({
-                    className,
-                    ...props
-                  }: ButtonProps) => (
-                    <Button
-                      intent="secondary"
-                      className={cn(
-                        "text-white border-[#FAFAFC] border hover:!bg-white/10",
-                        className
-                      )}
-                      {...props}
-                    >
-                      {cta.title}
-                    </Button>
-                  );
-                  return (
-                    <SetAReminderModal key={i} ModalButton={ReminderButton} />
-                  );
-                } else {
-                  return (
-                    <IconButton
-                      key={i}
-                      to={cta.href}
-                      className="text-white border-[#FAFAFC] border"
-                      withRotatingArrow={i === ctas.length - 1}
-                    >
-                      {cta.title}
-                    </IconButton>
-                  );
-                }
-              })}
-            </div>
+          className={cn(
+            "w-full max-w-screen-content mx-auto flex flex-col",
+            "gap-8 pb-8 md:gap-12 md:pb-16"
+          )}
+        >
+          <h1 className="font-extrabold heading-h1 text-[3rem] md:text-[4rem] lg:text-[100px] text-white">
+            {customTitle ? <HTMLRenderer html={customTitle} /> : pagePath}
+          </h1>
+          <div
+            role="separator"
+            aria-hidden="true"
+            className="hidden md:block h-[2px] self-stretch bg-[#D9D9D9] opacity-50"
+          />
+          <div className="flex items-center justify-between self-stretch">
+            {/* Breadcrumbs */}
+            <div className="flex flex-col gap-3 w-full md:px-0 md:flex-row md:items-center md:justify-between md:gap-0">
+              <div className="hidden lg:block">
+                <Breadcrumbs mode="light" />
+              </div>
+              <div
+                role="separator"
+                aria-hidden="true"
+                className="md:hidden h-[2px] self-stretch bg-[#D9D9D9]"
+              />
+              {/* Desktop CTAs */}
+              <div className="hidden lg:flex mt-5 flex-wrap justify-between gap-3 pr-1">
+                {ctas?.map((cta, i) => {
+                  if (cta.isSetAReminder) {
+                    const ReminderButton = ({
+                      className,
+                      ...props
+                    }: ButtonProps) => (
+                      <Button
+                        intent="secondary"
+                        className={cn(
+                          "text-white border-[#FAFAFC] border hover:!bg-white/10",
+                          className
+                        )}
+                        {...props}
+                      >
+                        {cta.title}
+                      </Button>
+                    );
+                    return (
+                      <SetAReminderModal key={i} ModalButton={ReminderButton} />
+                    );
+                  } else {
+                    return (
+                      <IconButton
+                        key={i}
+                        to={cta.href}
+                        className="text-white border-[#FAFAFC] border"
+                        withRotatingArrow={i === ctas.length - 1}
+                      >
+                        {cta.title}
+                      </IconButton>
+                    );
+                  }
+                })}
+              </div>
 
-            {/* Mobile CTAs */}
-            <div className="lg:hidden flex flex-col-reverse md:flex-row-reverse md:justify-end gap-3 w-full pt-8 md:pt-0 md:px-0">
-              {ctas?.map((cta, i) => {
-                if (cta.isSetAReminder) {
-                  const ReminderButton = ({
-                    className,
-                    ...props
-                  }: ButtonProps) => (
-                    <Button
-                      intent={
-                        i === 0 && ctas.length > 1 ? "secondary" : "primary"
-                      }
-                      className={cn(
-                        `w-full md:w-auto ${
+              {/* Mobile CTAs */}
+              <div className="lg:hidden flex flex-col-reverse md:flex-row-reverse md:justify-end gap-3 w-full pt-8 md:pt-0 md:px-0">
+                {ctas?.map((cta, i) => {
+                  if (cta.isSetAReminder) {
+                    const ReminderButton = ({
+                      className,
+                      ...props
+                    }: ButtonProps) => (
+                      <Button
+                        intent={
+                          i === 0 && ctas.length > 1 ? "secondary" : "primary"
+                        }
+                        className={cn(
+                          `w-full md:w-auto ${
+                            i !== 0 ? "" : "text-white border-white"
+                          }`,
+                          className
+                        )}
+                        {...props}
+                      >
+                        {cta.title}
+                      </Button>
+                    );
+                    return (
+                      <SetAReminderModal key={i} ModalButton={ReminderButton} />
+                    );
+                  } else {
+                    return (
+                      <Button
+                        key={i}
+                        intent={
+                          i === 0 && ctas.length > 1 ? "secondary" : "primary"
+                        }
+                        href={cta.href}
+                        className={`w-full md:w-auto ${
                           i !== 0 ? "" : "text-white border-white"
-                        }`,
-                        className
-                      )}
-                      {...props}
-                    >
-                      {cta.title}
-                    </Button>
-                  );
-                  return (
-                    <SetAReminderModal key={i} ModalButton={ReminderButton} />
-                  );
-                } else {
-                  return (
-                    <Button
-                      key={i}
-                      intent={
-                        i === 0 && ctas.length > 1 ? "secondary" : "primary"
-                      }
-                      href={cta.href}
-                      className={`w-full md:w-auto ${
-                        i !== 0 ? "" : "text-white border-white"
-                      }`}
-                    >
-                      {cta.title}
-                    </Button>
-                  );
-                }
-              })}
+                        }`}
+                      >
+                        {cta.title}
+                      </Button>
+                    );
+                  }
+                })}
+              </div>
             </div>
           </div>
         </div>
