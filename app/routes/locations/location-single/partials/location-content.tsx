57c8a2db4cf2e86d8888c2c4c63de744c0290eff
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-05-29 14:51:59 -0400

    Reapply "Started Tabs Component and added Sunday Details Tab"
    This reverts commit ff07221b683ba911b0a5bf07b99b9831e53ef413.

--- app/routes/locations/location-single/partials/location-content.tsx
+++ app/routes/locations/location-single/partials/location-content.tsx
@@ -2,6 +2,11 @@ import { CampusInfo } from "./campus-info.partial";
 import { LocationFAQ } from "./faq.partial";
 import { DynamicHero } from "~/components/dynamic-hero";
 import { LocationHitType } from "../types";
+import { CampusTabs } from "../components/tabs-component/campus-tabs.component";
+import { AboutUs } from "./tabs/about-us";
+import { SundayDetails } from "./tabs/sunday-details";
+import { UpcomingEvents } from "./tabs/upcoming-events";
+import { ForFamilies } from "./tabs/families";
 
 export function LocationSingle({ hit }: { hit: LocationHitType }) {
   if (!hit) {
@@ -12,8 +17,6 @@ export function LocationSingle({ hit }: { hit: LocationHitType }) {
     campusName,
     campusLocation,
     digitalTourVideo,
-    mapLink,
-    mapUrl,
     phoneNumber,
     serviceTimes,
     setReminderVideo,
@@ -23,6 +26,7 @@ export function LocationSingle({ hit }: { hit: LocationHitType }) {
     backgroundVideoMobile,
     backgroundVideoDesktop,
   } = hit;
+  // TODO: Figure out Spanish campus translation?
   const isEspanol = campusName?.includes("Español");
 
   return (
@@ -44,6 +48,10 @@ export function LocationSingle({ hit }: { hit: LocationHitType }) {
         campusLocation={campusLocation}
         digitalTourVideo={digitalTourVideo}
         weekdaySchedule={weekdaySchedule}
+      />
+      <CampusTabs
+        tabs={[SundayDetails, AboutUs, ForFamilies, UpcomingEvents]}
+        setReminderVideo={setReminderVideo}
       />
       <LocationFAQ campusName={campusName} />
     </div>
