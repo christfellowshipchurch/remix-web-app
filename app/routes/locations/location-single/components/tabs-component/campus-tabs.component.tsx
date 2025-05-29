72bea75bc08ce259b3150013dcf1d8e284dafe58
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-05-29 15:20:51 -0400

    Enhance Campus Tabs Component
    - Updated `CampusTabs` component styles for improved visual feedback with hover effects and animations.
    - Renamed `MobileTabs` to `MobileTabsDropdown` for better clarity and consistency in naming.
    - Adjusted tab content animation for a smoother user experience.

--- app/routes/locations/location-single/components/tabs-component/campus-tabs.component.tsx
+++ app/routes/locations/location-single/components/tabs-component/campus-tabs.component.tsx
@@ -27,7 +27,7 @@ interface CampusTabsProps {
   setReminderVideo: string;
 }
 const tasListStyle =
-  "absolute top-[-2rem] left-1/2 -translate-x-1/2 items-center justify-center rounded-[1rem] bg-white";
+  "absolute z-1 top-[-2rem] left-1/2 -translate-x-1/2 items-center justify-center rounded-[1rem] bg-white";
 
 export const CampusTabs = ({ tabs, setReminderVideo }: CampusTabsProps) => {
   const [activeTab, setActiveTab] = useState("sunday-details");
@@ -46,7 +46,7 @@ export const CampusTabs = ({ tabs, setReminderVideo }: CampusTabsProps) => {
           <Tabs.Trigger
             key={index}
             value={tab.value}
-            className="px-6 py-2 text-text-secondary font-bold data-[state=active]:bg-ocean data-[state=active]:text-white rounded-[12px]"
+            className="px-6 py-2 text-text-secondary font-bold data-[state=active]:bg-ocean data-[state=active]:text-white rounded-[12px] transition-all duration-300 hover:bg-ocean hover:text-white"
           >
             {tab.label}
           </Tabs.Trigger>
@@ -54,13 +54,17 @@ export const CampusTabs = ({ tabs, setReminderVideo }: CampusTabsProps) => {
       </Tabs.List>
 
       {/* Mobile Tabs - Radix UI Select */}
-      <MobileTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      <MobileTabsDropdown activeTab={activeTab} setActiveTab={setActiveTab} />
 
       <div className="w-full flex flex-col justify-center items-center">
         {tabData.map((tab, index) => {
           const TabComponent = tabs[index];
           return (
-            <Tabs.Content key={index} value={tab.value} className="w-full">
+            <Tabs.Content
+              key={index}
+              value={tab.value}
+              className="w-full data-[state=active]:animate-in data-[state=active]:fade-in data-[state=active]:zoom-in-95 data-[state=active]:duration-300"
+            >
               {TabComponent && (
                 <TabComponent setReminderVideo={setReminderVideo} />
               )}
@@ -72,7 +76,7 @@ export const CampusTabs = ({ tabs, setReminderVideo }: CampusTabsProps) => {
   );
 };
 
-const MobileTabs = ({
+const MobileTabsDropdown = ({
   activeTab,
   setActiveTab,
 }: {
