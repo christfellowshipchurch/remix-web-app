1afa0fbe01ef365591a2c2aedebe47f2edfa06b9
Daniel Wood <Dzwood327@gmail.com>
2025-05-30 16:14:07 -0400

    Fixed Group Finder Filter Modal

--- app/primitives/Modal/modal.primitive.tsx
+++ app/primitives/Modal/modal.primitive.tsx
@@ -36,7 +36,7 @@ function ModalContent({
       <Dialog.Overlay className="fixed inset-0 bg-black/50 data-[state=closed]:animate-dialogOverlayHide data-[state=open]:animate-dialogOverlayShow z-499" />
       <Dialog.Content
         className={cn(
-          "fixed sm:h-auto max-h-[90vh] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 data-[state=closed]:animate-dialogContentHide data-[state=open]:animate-dialogContentShow z-500 w-screen content-padding"
+          "fixed sm:h-auto max-h-[90vh] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 data-[state=closed]:animate-dialogContentHide data-[state=open]:animate-dialogContentShow z-500 content-padding"
         )}
       >
         <div
