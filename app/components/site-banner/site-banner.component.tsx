7c31cdd04fc2c1c86c14b5cff830dcfd6b4873a1
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-06-02 16:37:17 -0400

    updated banner

--- app/components/site-banner/site-banner.component.tsx
+++ app/components/site-banner/site-banner.component.tsx
@@ -11,10 +11,10 @@ export const SiteBanner = ({
   return (
     <div className="bg-ocean content-padding flex items-center justify-center w-full sticky top-0 z-999">
       <div className="text-white text-center py-2 flex justify-between items-center w-full">
-        <HTMLRenderer html={content || ""} />
+        <HTMLRenderer html={content || ""} className="w-full" />
 
         <div className="ml-auto cursor-pointer" onClick={onClose}>
-          <Icon name="x" />
+          <Icon name="x" size={32} />
         </div>
       </div>
     </div>
