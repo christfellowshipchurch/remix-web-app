c2726c9e9dba49ae8f939fb3e8307268da56bd8f
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-06-02 10:11:12 -0400

    Enhance location-single loader and families component
    - Updated the location-single loader to fetch and process children data, integrating Wistia and defined value fetching.
    - Improved error handling in the fetchChildren function to ensure robust data retrieval.
    - Modified the families component to utilize the updated familiesMappedChildren data for rendering.
    - Cleaned up unused code and comments for better readability and maintainability.

--- app/routes/locations/location-single/loader.ts
+++ app/routes/locations/location-single/loader.ts
@@ -1,6 +1,14 @@
 import { data, LoaderFunction } from "react-router";
 import { fetchRockData } from "~/lib/.server/fetch-rock-data";
-import { PageBuilderSection } from "~/routes/page-builder/types";
+import { fetchWistiaDataFromRock } from "~/lib/.server/fetch-wistia-data";
+import { createImageUrlFromGuid } from "~/lib/utils";
+import {
+  getSectionType,
+  isGuid,
+} from "~/routes/page-builder/components/builder-utils";
+import { fetchDefinedValue } from "~/routes/page-builder/loader";
+
+import { PageBuilderSection, SectionType } from "~/routes/page-builder/types";
 
 export type LoaderReturnType = {
   ALGOLIA_APP_ID: string;
@@ -8,47 +16,87 @@ export type LoaderReturnType = {
   GOOGLE_MAPS_API_KEY: string;
   campusName: string;
   url?: string;
-  familiesMappedChildren?: PageBuilderSection[];
+  familiesMappedChildren: PageBuilderSection[];
 };
 
 // const upcomingEventsContentIds = [19073, 19074, 19075, 19076, 19077, 19078];
 const familiesContentIds = [19038, 19072, 19047, 19048, 19046, 19071];
 
-// TODO: Fix this FetchRockData?
-// const fetchChildren = async ({
-//   contentIds,
-// }: {
-//   contentIds: number[];
-// }): Promise<any[]> => {
-//   let children;
-//   try {
-//     children = await Promise.all(
-//       contentIds.map(async (contentId: number) => {
-//         const child = await fetchRockData({
-//           endpoint: `ContentChannelItems`,
-//           queryParams: {
-//             $filter: `Id eq ${contentId}`,
-//             loadAttributes: "simple",
-//           },
-//         });
-//         console.log(child);
-//         return child;
-//       })
-//     );
-//   } catch (error) {
-//     console.error("Error fetching children:", error);
-//     children = [];
-//   }
+// TODO: Move all this to Algolia?
+const fetchChildren = async ({
+  contentIds,
+}: {
+  contentIds: number[];
+}): Promise<any[]> => {
+  let children;
+  try {
+    children = await Promise.all(
+      contentIds.map(async (contentId: number) => {
+        const child = await fetchRockData({
+          endpoint: `ContentChannelItems`,
+          queryParams: {
+            $filter: `Id eq ${contentId}`,
+            loadAttributes: "simple",
+          },
+        });
+        const attributeValues = Object.fromEntries(
+          Object.entries(child.attributeValues || {}).map(([key, obj]: any) => [
+            key,
+            obj.value,
+          ])
+        );
+        const sectionType = getSectionType(
+          child.contentChannelId
+        ) as SectionType;
+        child.type = sectionType;
 
-//   // Ensure children is an array
-//   let childrenArray = [];
-//   if (!Array.isArray(children)) {
-//     childrenArray = [children];
-//   } else {
-//     childrenArray = children;
-//   }
-//   return childrenArray;
-// };
+        if (sectionType === "CONTENT_BLOCK") {
+          const updatedValues = await Promise.all(
+            Object.entries(attributeValues || {}).map(async ([key, value]) => {
+              const processedValue =
+                typeof value === "string" &&
+                isGuid(value) &&
+                key !== "coverImage" &&
+                key !== "video"
+                  ? await fetchDefinedValue(value)
+                  : value;
+              return [key, processedValue];
+            })
+          );
+
+          const processedValues = Object.fromEntries(updatedValues);
+
+          const fetchVideo = attributeValues?.featureVideo
+            ? (await fetchWistiaDataFromRock(attributeValues.featureVideo))
+                .sourceKey
+            : null;
+
+          return {
+            ...child,
+            ...processedValues,
+            coverImage: createImageUrlFromGuid(attributeValues?.coverImage),
+            featureVideo: fetchVideo,
+            name: child.name || child.title,
+          };
+        }
+
+        return child;
+      })
+    );
+  } catch (error) {
+    console.error("Error fetching children:", error);
+    children = [];
+  }
+
+  // Ensure children is an array
+  let childrenArray = [];
+  if (!Array.isArray(children)) {
+    childrenArray = [children];
+  } else {
+    childrenArray = children;
+  }
+  return childrenArray;
+};
 
 export const loader: LoaderFunction = async ({ params }) => {
   const campusUrl = params.location;
@@ -69,24 +117,24 @@ export const loader: LoaderFunction = async ({ params }) => {
     });
   }
 
-  // const familiesChildren = await fetchChildren({
-  //   contentIds: familiesContentIds,
-  // });
+  const familiesChildren = await fetchChildren({
+    contentIds: familiesContentIds,
+  });
 
-  // // Also look throught the all Children and if the id of the child is 19046, replace it's title with "For Students"
-  // const familiesChildrenArray = familiesChildren.map((child) => {
-  //   if (child.id === 19046) {
-  //     child.name = "For Students";
-  //   }
-  //   return child;
-  // });
+  // Also look throught the all Children and if the id of the child is 19046, replace it's title with "For Students"
+  const familiesChildrenArray = familiesChildren.map((child) => {
+    if (child.id === 19046) {
+      child.name = "For Students";
+    }
+    return child;
+  });
 
   const pageData: LoaderReturnType = {
     ALGOLIA_APP_ID: appId,
     ALGOLIA_SEARCH_API_KEY: searchApiKey,
     GOOGLE_MAPS_API_KEY: googleMapsApiKey,
     campusName: decodeURIComponent(campusUrl),
-    // familiesMappedChildren: familiesChildrenArray,
+    familiesMappedChildren: familiesChildrenArray,
   };
 
   return pageData;
