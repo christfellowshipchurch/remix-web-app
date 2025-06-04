0d0a2472ed19fcea0d1e6b3874c881615a12d423
Daniel Wood <Dzwood327@gmail.com>
2025-06-04 10:36:04 -0400

    GTM Provider

---
+++ app/providers/gtm-provider/index.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
+import { useLocation } from "react-router";
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, unknown>[];
+  }
+}
+export {};
+
+interface GTMProviderProps {
+  children: React.ReactNode;
+  gtmId: string;
+}
+
+export function GTMProvider({ children, gtmId }: GTMProviderProps) {
+  const location = useLocation();
+
+  useEffect(() => {
+    TagManager.initialize({ gtmId });
+  }, [gtmId]);
+
+  useEffect(() => {
+    // Push pageview event on route change
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return <>{children}</>;
+}
