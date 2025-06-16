cf2ece99360d81c55bcddb6dfcbec19978209a41
Daniel Wood <Dzwood327@gmail.com>
2025-06-16 10:02:20 -0400

    Fixed Background Image and added Mobile Linking

--- app/routes/volunteer/partials/volunteer-feature-event.partial.tsx
+++ app/routes/volunteer/partials/volunteer-feature-event.partial.tsx
@@ -1,6 +1,7 @@
 import { IconButton } from "~/primitives/button/icon-button.primitive";
 import { useLoaderData } from "react-router";
 import type { LoaderReturnType } from "../loader";
+import { cn } from "~/lib/utils";
 
 export function VolunteerFeaturedEvent() {
   const { mockVolunteerFeaturedEvent } = useLoaderData<LoaderReturnType>();
@@ -8,13 +9,15 @@ export function VolunteerFeaturedEvent() {
     mockVolunteerFeaturedEvent;
   return (
     <section id="featured-event">
-      <div className="w-full bg-white content-padding relative hidden md:block">
-        <div className="max-w-screen-content mx-auto">
-          <img
-            src={imageUrl}
-            alt={`Volunteers at ${title} event`}
-            className="w-full h-[550px] object-cover shadow absolute inset-0 z-0 left-40"
-          />
+      <div className="ml-auto w-[95vw] min-[1600px]:w-[90vw] bg-white relative hidden md:block">
+        <div
+          className={cn(
+            "bg-cover bg-no-repeat",
+            `bg-[url('${imageUrl}')]`,
+            "md:bg-[right_0px_top_-80px]",
+            "lg:bg-[right_0px_top_-200px]"
+          )}
+        >
           <div className="relative inline-flex flex-col gap-4 bg-white p-12 z-10 lg:-ml-12 top-50">
             <span className="text-ocean font-semibold mb-2 block">
               {subtitle}
@@ -38,16 +41,18 @@ export function VolunteerFeaturedEvent() {
       {/* Mobile */}
       <div className="w-full bg-gray content-padding relative block md:hidden py-20">
         <div className="max-w-screen-content mx-auto rounded-4xl overflow-hidden">
-          <img
-            src={imageUrl}
-            alt={`Volunteers at ${title} event`}
-            className="w-full object-cover max-h-44 sm:max-h-64"
-          />
-          <div className="flex flex-col py-10 gap-4 bg-white p-6 z-10">
-            <span className="text-ocean font-bold block">{subtitle}</span>
-            <h2 className="text-3xl md:text-4xl font-bold">{title}</h2>
-            <p className="text-text-secondary text-pretty">{description}</p>
-          </div>
+          <a href={url} className="block">
+            <img
+              src={imageUrl}
+              alt={`Volunteers at ${title} event`}
+              className="w-full object-cover max-h-44 sm:max-h-64"
+            />
+            <div className="flex flex-col py-10 gap-4 bg-white p-6 z-10">
+              <span className="text-ocean font-bold block">{subtitle}</span>
+              <h2 className="text-3xl md:text-4xl font-bold">{title}</h2>
+              <p className="text-text-secondary text-pretty">{description}</p>
+            </div>
+          </a>
         </div>
       </div>
     </section>
