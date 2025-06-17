08afc0a04daa3de84e76b94352ffd3146c0aec5c
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-06-17 16:38:39 -0400

    classes start

---
+++ app/routes/class-finder/class-single/loader.tsx
@@ -0,0 +1,29 @@
+import { LoaderFunctionArgs } from "react-router";
+import { AuthenticationError } from "~/lib/.server/error-types";
+
+export type LoaderReturnType = {
+  ALGOLIA_APP_ID: string;
+  ALGOLIA_SEARCH_API_KEY: string;
+  groupName: string;
+};
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const title = params.path || "";
+
+  if (!title) {
+    throw new Error("Group not found");
+  }
+
+  const appId = process.env.ALGOLIA_APP_ID;
+  const searchApiKey = process.env.ALGOLIA_SEARCH_API_KEY;
+
+  if (!appId || !searchApiKey) {
+    throw new AuthenticationError("Algolia credentials not found");
+  }
+
+  return {
+    ALGOLIA_APP_ID: appId,
+    ALGOLIA_SEARCH_API_KEY: searchApiKey,
+    groupName: decodeURIComponent(title),
+  };
+}
---
+++ app/routes/class-finder/route.tsx
@@ -0,0 +1,6 @@
+import { ClassFinderPage } from "./finder/class-finder";
+
+export { action } from "./finder/action";
+export { loader } from "./finder/loader";
+
+export default ClassFinderPage;
