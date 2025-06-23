18bbc0025e77f2094ef93380bbc1926cc1727fa6
Daniel Wood <Dzwood327@gmail.com>
2025-06-23 17:19:49 -0400

    Added Form Navbar

--- app/routes/volunteer-form/route.tsx
+++ app/routes/volunteer-form/route.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import React from "react";
 import { cn } from "~/lib/utils";
+import { VolunteerFormNav } from "~/routes/volunteer-form/components/volunteer-form-nav.component";
 
 // Step 1: Welcome
 // Step 2: Personal Info
@@ -10,8 +11,12 @@ import { cn } from "~/lib/utils";
 
 export const VolunteerFormRoute: React.FC = () => {
   const location = useLocation();
-  const currentStep = location.pathname.split("/").pop();
+  const pathSegments = location.pathname.split("/");
+  const currentStep = pathSegments[pathSegments.length - 1];
   const isWelcomeStep = currentStep === "welcome";
+  const isFormPage = ["personal-info", "availability", "preferences"].includes(
+    currentStep
+  );
 
   return (
     <div
@@ -20,7 +25,7 @@ export const VolunteerFormRoute: React.FC = () => {
         isWelcomeStep ? "bg-navy" : "bg-white"
       )}
     >
-      {/* TODO: Add header/navigation for steps */}
+      {isFormPage && <VolunteerFormNav currentStepId={currentStep} />}
       <main
         className={cn(
           "flex-1 flex flex-col items-center",
