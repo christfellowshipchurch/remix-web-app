eb8ea4f5adeb081e527025299ea7bf6d120af1c2
Daniel Wood <Dzwood327@gmail.com>
2025-06-24 16:47:56 -0400

    Added in the Meantime section

--- app/routes/volunteer-form/partials/form-confirmation.partial.tsx
+++ app/routes/volunteer-form/partials/form-confirmation.partial.tsx
@@ -3,6 +3,8 @@ import { Form } from "react-router-dom";
 import { ResultsProgressBar } from "../components/results-progress-bar.component";
 import { ResultCard } from "../components/result-card.component";
 import { mockResultCards } from "../mock-data";
+import { HeroNavCard } from "~/components/navbar/desktop/nav-cards.component";
+import Icon from "~/primitives/icon";
 
 export const VolunteerFormConfirmationPartial: React.FC = () => (
   <section className="w-full min-h-screen bg-gray-100">
@@ -57,16 +59,32 @@ export const VolunteerFormConfirmationPartial: React.FC = () => (
     </div>
 
     {/* Finish Button */}
-    <div className="flex justify-center py-8 bg-white">
-      <Form method="post">
-        <button
-          type="submit"
-          className="px-8 py-3 bg-ocean text-white rounded font-semibold text-lg hover:bg-ocean/90 transition-colors shadow-md"
-          aria-label="Finish Volunteer Form"
-        >
-          Finish
-        </button>
-      </Form>
+    <div className="flex flex-col items-center justify-center gap-8 py-20 bg-white">
+      <h2 className="heading-h2 text-center">In the Meantime</h2>
+      <p className="text-center text-text-secondary max-w-2xl mx-auto">
+        The journey is an essential first step to getting involved and is
+        crucial for many dream team roles. If you haven't started yet, we
+        encourage you to begin this process, as it will help orient you to our
+        church.
+      </p>
+      <a
+        href="/journey"
+        className="bg-white p-4 rounded-lg shadow-medium hover:translate-y-[-6px] transition-all duration-300 max-w-96 border-1 border-neutral-lighter"
+      >
+        <img
+          src="https://picsum.photos/320/280"
+          alt="Start Your Journey"
+          className="w-80 rounded-lg aspect-[4/3]"
+        />
+        <h5 className="font-medium uppercase text-text-secondary text-xs mt-2">
+          the best first step
+        </h5>
+        <h3 className="heading-h5 text-text-primary">The Journey</h3>
+        <div className="mt-3 font-semibold flex w-full justify-between text-ocean">
+          <span>Save Your Spot</span>
+          <Icon name="arrowRight" />
+        </div>
+      </a>
     </div>
   </section>
 );
