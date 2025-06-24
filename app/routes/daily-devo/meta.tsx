5e5559f915ab4c8b0eb0b7c9b2e74f5310ffda4f
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-06-24 10:36:27 -0400

    daily devo

---
+++ app/routes/daily-devo/meta.tsx
@@ -0,0 +1,11 @@
+import { MetaFunction } from "react-router";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Daily Devotional | Christ Fellowship Church" },
+    {
+      name: "description",
+      content: "Daily Devotional from Christ Fellowship Church.",
+    },
+  ];
+};
