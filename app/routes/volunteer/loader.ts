e99fab27079d37d0687ff20ace9011f5c0cdcad5
Daniel Wood <Dzwood327@gmail.com>
2025-06-30 16:31:25 -0400

    Updated loader to support new Mission Trips

--- app/routes/volunteer/loader.ts
+++ app/routes/volunteer/loader.ts
@@ -1,17 +1,8 @@
 import { LoaderFunctionArgs } from "react-router";
-import {
-  CommunityCard,
-  RegionCard,
-  Trip,
-  VolunteerFeaturedEvent,
-} from "./types";
+import { CommunityCard, Trip, VolunteerFeaturedEvent } from "./types";
 import { fetchRockData } from "~/lib/.server/fetch-rock-data";
 import { createImageUrlFromGuid } from "~/lib/utils";
-import {
-  mockCommunityData,
-  mockRegionData,
-  mockVolunteerFeaturedEvent,
-} from "./mock-data";
+import { mockCommunityData, mockVolunteerFeaturedEvent } from "./mock-data";
 
 const fetchMissionTrips = async () => {
   const missionTrips = await fetchRockData({
@@ -20,6 +11,7 @@ const fetchMissionTrips = async () => {
       $filter: "ContentChannelId eq 174",
       loadAttributes: "simple",
     },
+    cache: false,
   });
 
   // ensure contentItems is an array
@@ -29,9 +21,8 @@ const fetchMissionTrips = async () => {
 };
 
 export type LoaderReturnType = {
-  missionTrips: Trip[];
+  missionTrips: Record<string, Trip[]>;
   mockCommunityData: CommunityCard[];
-  mockRegionData: RegionCard[];
   mockVolunteerFeaturedEvent: VolunteerFeaturedEvent;
 };
 
@@ -43,17 +34,35 @@ export async function loader({ request }: LoaderFunctionArgs) {
     title: item.title,
     description: item.content,
     coverImage: createImageUrlFromGuid(item.attributeValues?.coverImage.value),
-    missionTripUrl: item.attributeValues?.url.value,
+    applyUrl: item.attributeValues?.applyUrl.value,
+    donateUrl: item.attributeValues?.donateUrl.value,
+    groupType: item.attributeValues?.groupType.value,
+    city: item.attributeValues?.city.value,
+    country: item.attributeValues?.country.value,
+    dateOfTrip: item.attributeValues?.dateOfTrip.value,
+    cost: item.attributeValues?.cost.value,
     coordinates: {
       lat: Number(item.attributeValues?.latitude.value) || 0,
       lng: Number(item.attributeValues?.longitude.value) || 0,
     },
   }));
 
+  // Group trips by country
+  const groupedTrips = missionTrips.reduce(
+    (acc: Record<string, Trip[]>, trip: Trip) => {
+      const country = trip.country;
+      if (!acc[country]) {
+        acc[country] = [];
+      }
+      acc[country].push(trip);
+      return acc;
+    },
+    {} as Record<string, Trip[]>
+  );
+
   return Response.json({
-    missionTrips,
+    missionTrips: groupedTrips,
     mockCommunityData,
-    mockRegionData,
     mockVolunteerFeaturedEvent,
   } as LoaderReturnType);
 }
