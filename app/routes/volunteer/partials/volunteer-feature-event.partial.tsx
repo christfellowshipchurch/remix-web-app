163032800406484a6f82eb40006daa44de42d599
Daniel Wood <Dzwood327@gmail.com>
2025-07-10 16:19:07 -0400

    Started Initial Setup

--- app/routes/volunteer/partials/volunteer-feature-event.partial.tsx
+++ app/routes/volunteer/partials/volunteer-feature-event.partial.tsx
@@ -2,16 +2,16 @@ import { IconButton } from "~/primitives/button/icon-button.primitive";
 import { useLoaderData } from "react-router-dom";
 import type { LoaderReturnType } from "../loader";
 import { cn } from "~/lib/utils";
+import HTMLRenderer from "~/primitives/html-renderer";
 
 export function VolunteerFeaturedEvent() {
-  const { mockVolunteerFeaturedEvent } = useLoaderData<LoaderReturnType>();
-  const { title, subtitle, description, url, imageUrl } =
-    mockVolunteerFeaturedEvent;
+  const { featuredEvent } = useLoaderData<LoaderReturnType>();
+  const { title, content, attributeValues } = featuredEvent;
   return (
     <section id="featured-event">
       <div className="ml-auto w-[95vw] min-[1600px]:w-[90vw] bg-white relative hidden md:block">
         <div
-          style={{ backgroundImage: `url(${imageUrl})` }}
+          style={{ backgroundImage: `url(${attributeValues.image.value})` }}
           className={cn(
             "bg-cover bg-no-repeat",
             "md:bg-[right_0px_top_-80px]",
@@ -20,17 +20,17 @@ export function VolunteerFeaturedEvent() {
         >
           <div className="relative inline-flex flex-col gap-4 bg-white p-12 z-10 lg:-ml-12 top-50">
             <span className="text-ocean font-semibold mb-2 block">
-              {subtitle}
+              {attributeValues.campus?.value}
             </span>
             <h2 className="text-3xl md:text-4xl font-bold mb-4">{title}</h2>
-            <p className="text-neutral-dark mb-8 max-w-[490px]">
-              {description}
-            </p>
+            <div className="text-neutral-dark mb-8 max-w-[490px] line-clamp-5">
+              <HTMLRenderer html={content || ""} stripFormatting />
+            </div>
             <IconButton
-              to={url}
+              to={attributeValues.url.value}
               withRotatingArrow
               className="bg-white border-neutral-default text-neutral-dark hover:enabled:bg-soft-white hover:enabled:text-neutral-dark"
-              aria-label={`Learn more about ${title}`}
+              aria-label={`Learn more about ${attributeValues.campus?.value}`}
             >
               Learn More
             </IconButton>
@@ -41,16 +41,20 @@ export function VolunteerFeaturedEvent() {
       {/* Mobile */}
       <div className="w-full bg-gray content-padding relative block md:hidden py-20">
         <div className="max-w-screen-content mx-auto rounded-4xl overflow-hidden">
-          <a href={url} className="block">
+          <a href={attributeValues.url.value} className="block">
             <img
-              src={imageUrl}
-              alt={`Volunteers at ${title} event`}
+              src={attributeValues.image.value}
+              alt={`Volunteers at ${attributeValues.campus?.value} event`}
               className="w-full object-cover max-h-44 sm:max-h-64"
             />
+            {attributeValues.campus?.value && (
+              <span className="text-ocean font-bold block">
+                {attributeValues.campus.value}
+              </span>
+            )}
             <div className="flex flex-col py-10 gap-4 bg-white p-6 z-10">
-              <span className="text-ocean font-bold block">{subtitle}</span>
               <h2 className="text-3xl md:text-4xl font-bold">{title}</h2>
-              <p className="text-text-secondary text-pretty">{description}</p>
+              <p className="text-text-secondary text-pretty">{content}</p>
             </div>
           </a>
         </div>
