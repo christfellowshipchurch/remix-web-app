b87b2638df19dedef52d38a366643e511afdbcca
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-07-10 16:42:28 -0400

    updated comment

--- app/routes/volunteer-form/loader.ts
+++ app/routes/volunteer-form/loader.ts
@@ -2,7 +2,7 @@ import { LoaderFunctionArgs, redirect } from "react-router-dom";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
-  // If the path is exactly /yes (no trailing slash or anything after)
+  // If the path is exactly /volunteer-form (no trailing slash or anything after)
   if (url.pathname === "/volunteer-form") {
     return redirect("/volunteer-form/welcome");
   }
