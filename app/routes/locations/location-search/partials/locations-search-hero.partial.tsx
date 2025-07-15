c5a712f346446a0597f429a95cd0f15c3f124214
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-07-15 09:58:48 -0400

    location search update

--- app/routes/locations/location-search/partials/locations-search-hero.partial.tsx
+++ app/routes/locations/location-search/partials/locations-search-hero.partial.tsx
@@ -1,39 +1,45 @@
-import { Button } from "~/primitives/button/button.primitive";
 import Icon from "~/primitives/icon";
-import { CampusesReturnType } from "../loader";
-import * as Form from "@radix-ui/react-form";
 import { useLoaderData } from "react-router-dom";
-import { defaultTextInputStyles } from "~/primitives/inputs/text-field/text-field.primitive";
 import { useEffect, useState } from "react";
 import { Video } from "~/primitives/video/video.primitive";
+import { cn, isValidZip } from "~/lib/utils";
+import { SearchBox, useSearchBox } from "react-instantsearch";
 
 type SearchProps = {
-  setAddress: (address: string) => void;
-  getCoordinates: () => void;
-  searchCurrentLocation: () => void;
-  locationActive: boolean;
-  error?: string | null;
+  handleSearch: (query: string | null) => void;
+  setCoordinates: (coordinates: { lat: number; lng: number }) => void;
 };
 
-export const Search = ({
-  setAddress,
-  searchCurrentLocation,
-  getCoordinates,
-  locationActive,
-  error,
-}: SearchProps) => {
-  const [serverErrors, setServerErrors] = useState(false);
-  const { bgVideo } = useLoaderData<CampusesReturnType>();
-  const zipCodeRegex = /^[0-9]{5}(?:-[0-9]{4})?$/;
+export const Search = ({ handleSearch, setCoordinates }: SearchProps) => {
+  const { bgVideo } = useLoaderData();
 
+  const [useCurrentLocation, setUseCurrentLocation] = useState(false);
+  const [locationActive, setLocationActive] = useState(true);
+
+  // Set the coordinates to the user's current location
   useEffect(() => {
-    if (error) {
-      setServerErrors(true);
+    if (useCurrentLocation) {
+      // Get the current location
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCoordinates({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+          setLocationActive(true);
+        },
+        (error) => {
+          console.error(error);
+          setLocationActive(false);
+        }
+      );
+
+      setUseCurrentLocation(false);
     }
-  }, [error]);
+  }, [useCurrentLocation]);
 
   return (
-    <div className="flex h-[70vh] w-full items-center justify-center md:h-[78vh]">
+    <div className="flex h-[80vh] w-full items-center justify-center md:h-[78vh]">
       <div className="relative flex size-full overflow-hidden">
         <Video
           src={bgVideo}
@@ -43,56 +49,22 @@ export const Search = ({
           className="absolute left-0 top-0 size-full object-cover"
         />
         <div className="absolute size-full bg-[rgba(0,0,0,0.5)]" />
-        <div className="absolute left-1/2 top-1/2 flex w-full max-w-[90vw] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-6 rounded-xl bg-black/45 py-12 text-center text-white backdrop-blur lg:max-w-[900px]">
-          <h1 className="text-[36px] font-bold">
+        <div className="absolute left-1/2 top-1/2 flex w-full max-w-[90vw] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-4 md:gap-6 rounded-xl md:bg-black/45 py-12 text-center text-white md:backdrop-blur lg:max-w-[900px]">
+          <h1 className="text-3xl leading-tight md:text-[36px] font-bold">
             Christ Fellowship Church Locations
           </h1>
-          <p className="max-w-[90vw] text-xl md:max-w-[560px]">
+          <p className="max-w-[90vw] md:text-xl md:max-w-[560px]">
             Christ Fellowship is one church with many locations across South
             Florida, and online—wherever you are!
           </p>
-          <Form.Root
-            onSubmit={(e) => {
-              e.preventDefault();
-              getCoordinates();
-            }}
-            className="flex flex-col items-center gap-2.5"
-          >
-            <Form.Field name="zipcode" serverInvalid={serverErrors}>
-              <Form.Control
-                asChild
-                className="flex w-[60vw] justify-between md:w-[440px] "
-              >
-                <input
-                  className={`${defaultTextInputStyles} text-center text-white focus:!border-neutral-500`}
-                  placeholder="Enter zip code here"
-                  onChange={(e) => {
-                    setAddress(e.target.value);
-                    setServerErrors(false);
-                  }}
-                />
-              </Form.Control>
-              <Form.Message
-                className="text-sm text-alert"
-                match={(value) => !zipCodeRegex.test(value)}
-              >
-                Please enter valid a zipcode
-              </Form.Message>
-              {serverErrors && (
-                <Form.Message className="text-sm text-alert">
-                  {error}
-                </Form.Message>
-              )}
-            </Form.Field>
-            <Button type="submit" size="md">
-              Find a Location
-            </Button>
-          </Form.Root>
+
+          <SearchBar onSearchSubmit={handleSearch} data-gtm="hero-cta" />
+
           <div className="flex flex-col items-center gap-2">
             <div className="flex gap-2">
               <div
                 className="cursor-pointer italic underline"
-                onClick={() => searchCurrentLocation()}
+                onClick={() => setUseCurrentLocation(true)}
               >
                 Use my current location
               </div>
@@ -106,6 +78,62 @@ export const Search = ({
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+
+const SearchBar = ({
+  onSearchSubmit,
+}: {
+  onSearchSubmit: (query: string | null) => void;
+}) => {
+  const { query, refine } = useSearchBox();
+  const [zipCode, setZipCode] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (query.length === 5 && isValidZip(query)) {
+      setZipCode(query);
+    }
+  }, [query]);
+
+  // When the zip code is set, search for it and clear the query so the query doesn't interfere with the geo search
+  useEffect(() => {
+    if (zipCode) {
+      onSearchSubmit(zipCode);
+      refine("");
+    }
+  }, [zipCode]);
+
+  return (
+    <div
+      className={cn(
+        "flex w-full md:max-w-[500px] items-center gap-4 rounded-full p-1 mt-2 md:mt-0",
+        query ? "bg-gray" : "bg-white"
+      )}
+    >
+      <button
+        type="submit"
+        className="flex items-center justify-center p-2 bg-ocean lg:bg-dark-navy rounded-full relative"
+      >
+        <Icon
+          name="search"
+          size={20}
+          className={`text-white relative right-[1px] bottom-[1px]`}
+        />
+      </button>
+
+      <SearchBox
+        placeholder="Search by zip code"
+        classNames={{
+          root: "flex-grow w-full",
+          form: "flex",
+          input: `w-full justify-center text-black px-3 outline-none appearance-none`,
+          reset: "hidden",
+          loadingIndicator: "hidden",
+          resetIcon: "hidden",
+          submit: "hidden",
+        }}
+      />
     </div>
   );
 };
