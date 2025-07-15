2aa0250b2422f65414799d962cd56e903bd131b9
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-07-15 10:09:51 -0400

    bg video update

--- app/routes/locations/location-search/loader.tsx
+++ app/routes/locations/location-search/loader.tsx
@@ -1,8 +1,5 @@
 import { LoaderFunctionArgs } from "react-router-dom";
-import { fetchWistiaData } from "~/lib/.server/fetch-wistia-data";
 
 export async function loader({ params }: LoaderFunctionArgs) {
-  const bgVideo = await fetchWistiaData({ id: "padj4c4xoh", size: 960 });
-
-  return { bgVideo };
+  return {};
 }
