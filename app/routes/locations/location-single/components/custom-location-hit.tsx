c480fc822d154f46358ec30f0ad1cd89bacd016a
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-08-13 12:27:18 -0400

    moved to loader

--- app/routes/locations/location-single/components/custom-location-hit.tsx
+++ app/routes/locations/location-single/components/custom-location-hit.tsx
@@ -5,13 +5,7 @@ import { useLoaderData } from "react-router-dom";
 import { LoaderReturnType } from "../loader";
 
 export const LocationPageHit = () => {
-  const { allCampuses, campusUrl } = useLoaderData<LoaderReturnType>();
   const { items } = useHits<LocationHitType>();
-
-  // Check if the current campus URL is in the list of valid campuses
-  if (!allCampuses.includes(campusUrl)) {
-    throw new Error("Location Not Found");
-  }
 
   return (
     <div className="w-full">
