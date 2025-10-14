7b514c8e3b1a25490a960e9243a55eccb948f1f6
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2025-10-14 16:04:20 -0400

    Adding bg image

--- app/routes/events/event-single/partials/hero.partial.tsx
+++ app/routes/events/event-single/partials/hero.partial.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom";
 import BulletPoints from "~/primitives/bullet-points";
 import { Button } from "~/primitives/button/button.primitive";
+import heroBgImgStyles from "~/styles/hero-bg-image-styles";
 
 export const EventsSingleHero = ({
   imagePath,
@@ -26,54 +27,58 @@ export const EventsSingleHero = ({
       )[0] || "Home";
 
   return (
-    <div className="w-full flex items-center content-padding py-8 md:py-12 lg:py-16">
-      <div className="flex flex-col-reverse md:flex-row w-full mx-auto gap-2 md:gap-8 lg:gap-16 xl:!justify-between items-center justify-center self-stretch max-w-screen-content md:mb-12">
-        {/* Left / Bottom Side (Desktop) */}
-        <div className="flex flex-col gap-10 mb-4 md:mb-0">
-          <div className="flex flex-col gap-3 max-w-[600px]">
-            <h1 className="font-extrabold heading-h4 md:heading-h3 lg:text-[52px] text-text-primary leading-tight !text-pretty -mb-1">
-              {customTitle || pagePath}
-            </h1>
+    <div className="w-full" style={heroBgImgStyles(imagePath)}>
+      <div className="bg-white/85 backdrop-blur-xl content-padding w-full">
+        <div className="w-full flex items-center content-padding py-8 md:py-12 lg:py-16">
+          <div className="flex flex-col-reverse md:flex-row w-full mx-auto gap-2 md:gap-8 lg:gap-16 xl:!justify-between items-center justify-center self-stretch max-w-screen-content md:mb-12">
+            {/* Left / Bottom Side (Desktop) */}
+            <div className="flex flex-col gap-10 mb-4 md:mb-0">
+              <div className="flex flex-col gap-3 max-w-[600px]">
+                <h1 className="font-extrabold heading-h4 md:heading-h3 lg:text-[52px] text-text-primary leading-tight !text-pretty -mb-1">
+                  {customTitle || pagePath}
+                </h1>
 
-            {subtitle && (
-              <p className="font-medium lg:text-lg text-[#717182] max-w-[420px]">
-                {subtitle}
-              </p>
-            )}
+                {subtitle && (
+                  <p className="font-medium lg:text-lg text-[#717182] max-w-[420px]">
+                    {subtitle}
+                  </p>
+                )}
 
-            {/* CTAs */}
-            <div className="mt-5 md:mt-0 flex flex-wrap gap-2 sm:gap-4">
-              {ctas?.map((cta, i) => (
-                <Button
-                  key={i}
-                  href={cta.href}
-                  intent={i === 0 ? "primary" : "secondary"}
-                >
-                  {cta.title}
-                </Button>
-              ))}
+                {/* CTAs */}
+                <div className="mt-5 md:mt-0 flex flex-wrap gap-2 sm:gap-4">
+                  {ctas?.map((cta, i) => (
+                    <Button
+                      key={i}
+                      href={cta.href}
+                      intent={i === 0 ? "primary" : "secondary"}
+                    >
+                      {cta.title}
+                    </Button>
+                  ))}
+                </div>
+              </div>
+
+              {/* Bullet Points */}
+              <BulletPoints
+                dotStyles="bg-navy size-[6px]"
+                textStyles="text-[#717182] text-sm font-medium"
+                className="gap-1 lg:gap-2"
+                points={[
+                  "Connect with others and build meaningful relationships",
+                  "Learn and grow through engaging content and discussions",
+                  "Make a positive impact in your community",
+                ]}
+              />
             </div>
+
+            {/* Right / Top Side (Mobile) */}
+            <img
+              src={imagePath}
+              alt={customTitle || pagePath}
+              className="w-full max-w-[90vw] md:max-w-[340px] lg:max-w-[480px] xl:!max-w-[638px] aspect-[638/478] object-cover rounded-[14px] shadow-xl"
+            />
           </div>
-
-          {/* Bullet Points */}
-          <BulletPoints
-            dotStyles="bg-navy size-[6px]"
-            textStyles="text-[#717182] text-sm font-medium"
-            className="gap-1 lg:gap-2"
-            points={[
-              "Connect with others and build meaningful relationships",
-              "Learn and grow through engaging content and discussions",
-              "Make a positive impact in your community",
-            ]}
-          />
         </div>
-
-        {/* Right / Top Side (Mobile) */}
-        <img
-          src={imagePath}
-          alt={customTitle || pagePath}
-          className="w-full max-w-[90vw] md:max-w-[340px] lg:max-w-[480px] xl:!max-w-[638px] aspect-[638/478] object-cover rounded-[14px] shadow-xl"
-        />
       </div>
     </div>
   );
