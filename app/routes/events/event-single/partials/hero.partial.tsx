000129c5302fd0cf1389141be35e2c23b64d781a
Daniel Wood <Dzwood327@gmail.com>
2025-10-30 16:15:08 -0400

    Fixed Hero Styling

--- app/routes/events/event-single/partials/hero.partial.tsx
+++ app/routes/events/event-single/partials/hero.partial.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useResponsive } from "~/hooks/use-responsive";
 import BulletPoints from "~/primitives/bullet-points";
 import { Button } from "~/primitives/button/button.primitive";
 import heroBgImgStyles from "~/styles/hero-bg-image-styles";
@@ -16,28 +16,18 @@ export const EventsSingleHero = ({
   subtitle?: string;
   quickPoints?: string[];
 }) => {
-  const location = useLocation();
-  const pagePath =
-    location.pathname
-      .split("/")
-      .filter(Boolean)
-      .map((segment) =>
-        segment
-          .split("-")
-          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-          .join(" ")
-      )[0] || "Home";
-
+  // viewport hook
+  const { isSmall, isMedium } = useResponsive();
   return (
     <div className="w-full" style={heroBgImgStyles(imagePath)}>
-      <div className="bg-white/85 backdrop-blur-xl content-padding w-full">
+      <div className="bg-white/85 backdrop-blur-xl w-full">
         <div className="w-full flex items-center content-padding py-8 md:py-12 lg:py-16">
-          <div className="flex flex-col-reverse md:flex-row w-full mx-auto gap-2 md:gap-8 lg:gap-16 xl:!justify-between items-center justify-center self-stretch max-w-screen-content md:mb-12">
+          <div className="grid grid-cols-1 md:grid-cols-2 w-full mx-auto gap-2 md:gap-8 lg:gap-16 place-items-center md:items-start self-stretch max-w-screen-content md:mb-12">
             {/* Left / Bottom Side (Desktop) */}
-            <div className="flex flex-col gap-10 mb-4 md:mb-0">
+            <div className="order-2 md:order-1 flex flex-col gap-10 mb-4 md:mb-0">
               <div className="flex flex-col gap-3 max-w-[600px]">
                 <h1 className="font-extrabold heading-h4 md:heading-h3 lg:text-[52px] text-text-primary leading-tight !text-pretty -mb-1">
-                  {customTitle || pagePath}
+                  {customTitle}
                 </h1>
 
                 {subtitle && (
@@ -47,12 +37,14 @@ export const EventsSingleHero = ({
                 )}
 
                 {/* CTAs */}
-                <div className="mt-5 md:mt-0 flex flex-wrap gap-2 sm:gap-4">
+                <div className="flex flex-col md:flex-wrap gap-2 sm:gap-4">
                   {ctas?.map((cta, i) => (
                     <Button
                       key={i}
                       href={cta.url}
                       intent={i === 0 ? "primary" : "secondary"}
+                      size={isSmall || isMedium ? "md" : "lg"}
+                      className="w-full md:w-auto md:w-auto"
                     >
                       {cta.title}
                     </Button>
@@ -74,8 +66,8 @@ export const EventsSingleHero = ({
             {/* Right / Top Side (Mobile) */}
             <img
               src={imagePath}
-              alt={customTitle || pagePath}
-              className="w-full max-w-[90vw] md:max-w-[340px] lg:max-w-[480px] xl:!max-w-[638px] aspect-[638/478] object-cover rounded-[14px] shadow-xl"
+              alt={customTitle}
+              className="order-1 md:order-2 w-full max-w-lg md:max-w-[340px] lg:max-w-[480px] xl:!max-w-[638px] aspect-[638/478] md:aspect-[5/6] lg:aspect-[638/478] object-cover rounded-[14px] shadow-xl"
             />
           </div>
         </div>
