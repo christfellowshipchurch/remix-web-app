963076723e71a4de7c8aa52418e6b8a5021807a6
Daniel Wood <Dzwood327@gmail.com>
2025-12-09 10:14:01 -0500

    Reorganized AllArticles/ArticleSingle
    Matches other routes like Messages for consistency.

--- app/routes/articles/all-articles/loader.ts
+++ app/routes/articles/all-articles/loader.ts
@@ -1,11 +1,21 @@
+import { LoaderFunction } from "react-router-dom";
+import { AuthenticationError } from "~/lib/.server/error-types";
+
 export type AllArticlesReturnType = {
-  ALGOLIA_APP_ID: string | undefined;
-  ALGOLIA_SEARCH_API_KEY: string | undefined;
+  ALGOLIA_APP_ID: string;
+  ALGOLIA_SEARCH_API_KEY: string;
 };
 
-export const loader = async (): Promise<AllArticlesReturnType> => {
+export const loader: LoaderFunction = async () => {
+  const appId = process.env.ALGOLIA_APP_ID;
+  const searchApiKey = process.env.ALGOLIA_SEARCH_API_KEY;
+
+  if (!appId || !searchApiKey) {
+    throw new AuthenticationError("Algolia credentials not found");
+  }
+
   return {
-    ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
-    ALGOLIA_SEARCH_API_KEY: process.env.ALGOLIA_SEARCH_API_KEY,
+    ALGOLIA_APP_ID: appId,
+    ALGOLIA_SEARCH_API_KEY: searchApiKey,
   };
 };
---
+++ app/routes/articles/all-articles/partials/all-articles.partial.tsx
@@ -0,0 +1,116 @@
+import { useLoaderData } from "react-router-dom";
+import { InstantSearch, Configure, useHits } from "react-instantsearch";
+import { useMemo, useState, useEffect } from "react";
+
+import { ContentItemHit } from "~/routes/search/types";
+import { CustomPagination } from "~/components/custom-pagination";
+import {
+  HubsTagsRefinementList,
+  HubsTagsRefinementLoadingSkeleton,
+} from "~/components/hubs-tags-refinement";
+import { createSearchClient } from "~/lib/create-search-client";
+import { AllArticlesReturnType } from "../loader";
+import { ArticleCard } from "../components/article-card.component";
+import { useResponsive } from "~/hooks/use-responsive";
+
+export function AllArticles() {
+  const { ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY } =
+    useLoaderData<AllArticlesReturnType>();
+
+  const searchClient = useMemo(
+    () =>
+      createSearchClient(ALGOLIA_APP_ID || "", ALGOLIA_SEARCH_API_KEY || ""),
+    [ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY]
+  );
+
+  const [allArticlesLoading, setAllArticlesLoading] = useState(true);
+  const { isSmall, isMedium, isLarge, isXLarge } = useResponsive();
+
+  const hitsPerPage = (() => {
+    switch (true) {
+      case isXLarge || isLarge:
+        return 12;
+      case isMedium:
+        return 8;
+      case isSmall:
+        return 8;
+      default:
+        return 12;
+    }
+  })();
+
+  return (
+    <section className="relative pb-28 pt-18 min-h-screen bg-white content-padding pagination-scroll-to">
+      <div className="relative max-w-screen-content mx-auto">
+        {allArticlesLoading && <AllArticlesLoadingSkeleton />}
+        <InstantSearch
+          indexName="dev_contentItems"
+          searchClient={searchClient}
+          future={{
+            preserveSharedStateOnUnmount: true,
+          }}
+        >
+          <Configure
+            filters={`contentType:"Article"`}
+            hitsPerPage={hitsPerPage}
+            distinct={true}
+          />
+
+          {/* Filter Section */}
+          <div className="mt-10 mb-12">
+            <HubsTagsRefinementList tagName="articlePrimaryCategories" />
+          </div>
+
+          {/* Results Grid */}
+          <AllArticlesHit setAllArticlesLoading={setAllArticlesLoading} />
+
+          <CustomPagination />
+        </InstantSearch>
+      </div>
+    </section>
+  );
+}
+
+const AllArticlesHit = ({
+  setAllArticlesLoading,
+}: {
+  setAllArticlesLoading: (allArticlesLoading: boolean) => void;
+}) => {
+  const { items } = useHits<ContentItemHit>();
+
+  useEffect(() => {
+    if (items.length > 0) {
+      setAllArticlesLoading(false);
+    }
+  }, [items, setAllArticlesLoading]);
+
+  if (items.length === 0) return null;
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-4 xl:!gap-8 justify-center items-center">
+      {items.map((hit) => (
+        <ArticleHit hit={hit} key={hit.objectID} />
+      ))}
+    </div>
+  );
+};
+
+const ArticleHit = ({ hit }: { hit: ContentItemHit }) => {
+  return <ArticleCard article={hit} />;
+};
+
+const AllArticlesLoadingSkeleton = () => {
+  return (
+    <div className="flex flex-col gap-8 pt-8">
+      <HubsTagsRefinementLoadingSkeleton />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-4 xl:!gap-8 justify-center items-center">
+        {[...Array(9)].map((_, i) => (
+          <div
+            key={i}
+            className="w-[460px] h-[360px] bg-gray-100 animate-pulse rounded-lg"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
