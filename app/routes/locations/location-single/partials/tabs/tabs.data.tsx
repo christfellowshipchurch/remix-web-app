3536f558faee4e8712a52b99c62c3477900b887a
Leandro Alfonso <Leandroalfonso14@gmail.com>
2026-01-29 11:37:20 -0500

    Update Spanish kids/students content & links
    Translate and refine kids/students content blocks and update CTA links. Spanish content block names, subtitles and HTML bodies were replaced with localized text (including updated wording for services, special needs, and CFStudents) and improved markup/classes. Calls-to-action were updated for both English and Spanish blocks to point to the students ministry page (/ministries/students) and the CFStudents YouTube channel, and some schedule wording was adjusted (e.g. Wednesday→Thursday in Spanish copy). Minor copy improvements were made to The MIX and check-in prompts to clarify times/registration.

--- app/routes/locations/location-single/partials/tabs/tabs.data.tsx
+++ app/routes/locations/location-single/partials/tabs/tabs.data.tsx
@@ -144,7 +144,8 @@ export const familiesMappedChildren: ContentBlockData[] = [
       "If you’re a middle or high school student, we’d love to see you on Wednesday nights at 6:30PM.",
     layoutType: "CARD",
     subtitle: "",
-    callsToAction: "Times and Info^/locations|Join Online^#youtube",
+    callsToAction:
+      "Times and Info^/ministries/students|Join Online^https://www.youtube.com/@cfstudentsfl",
     coverImage: undefined,
     featureVideo: undefined,
     aspectRatio: "16by9",
@@ -157,11 +158,36 @@ export const spanishFamiliesMappedChildren: ContentBlockData[] = [
   {
     id: "19038",
     type: "CONTENT_BLOCK",
-    name: "A Place for Every Kid",
-    content:
-      '<div style="color: #222222">Kids enjoy worship, group time lead by caring leaders, and a teaching designed to help your child love Jesus, love others, and love life.\u003Cdiv\u003E\u003Cbr\u003E\u003C/div\u003E\u003Cdiv\u003E\u003Cb\u003EBabies &amp; Preschool: God\'s Big Backyard\u003C/b\u003E\u003C/div\u003E\u003Cdiv\u003EOur prayer is that every preschool child would understand that God made them, He loves them, and Jesus wants to be their friend forever.\u003C/div\u003E\u003Cdiv\u003E\u003Cbr\u003E\u003C/div\u003E\u003Cdiv\u003E\u003Cb\u003EKindergarten - 5th Grade: The Studio&nbsp;\u003C/b\u003E\u003C/div\u003E\u003Cdiv\u003EWhere kids discover how God\'s Word relates to their lives, how to pray, and how to build lasting friendships.\u003C/div\u003E\u003Cdiv\u003E\u003Cbr\u003E\u003C/div\u003E\u003Cdiv\u003E\u003Cb\u003ESpecial Needs\u003C/b\u003E\u003C/div\u003E\u003Cdiv\u003EWe offer families the opportunity to pair their child with a volunteer "Buddy" while their child participates in the Christ Fellowship Kids environment.&nbsp;\u003C/div\u003E\u003Cdiv\u003E\u003Ca href="https://rock.gocf.org/page/2207" target="_blank"\u003ELEARN MORE HERE\u003C/a\u003E\u003C/div\u003E</div>',
+    name: "Un Lugar para Cada Niño",
+    content: `<div class="text-neutral-dark">
+        Los niños disfrutan de adoración, tiempo en grupos guiados por líderes atentos y una enseñanza diseñada para ayudar a tu hijo o hija a amar a Jesús, amar a los demás y amar la vida.
+        <div class="my-2"></div>
+
+        <br />
+        <b>Bebés y Preescolares: God’s Big Backyard</b>
+        <p>
+          Nuestra oración es que cada niño en edad preescolar entienda que Dios lo creó, que Él lo ama y que Jesús quiere ser su amigo para siempre.
+        </p>
+        <div class="my-2"></div>
+
+        <br />
+        <b>Kinder – 5.º Grado: The Studio</b>
+        <p>
+          Un espacio donde los niños descubren cómo la Palabra de Dios se relaciona con sus vidas, cómo orar y cómo construir amistades duraderas.
+        </p>
+        <div class="my-2"></div>
+
+
+        <br />
+        <b>Necesidades Especiales</b>
+        <p>
+          Ofrecemos a las familias la oportunidad de asignar a su hijo un voluntario <b>“Buddy”</b> mientras participa en el ambiente de <i>Christ Fellowship Kids</i>.
+        </p>
+
+        <a href="https://rock.gocf.org/page/2207" target="_blank" class="text-primary underline">APRENDE MÁS AQUÍ</a>
+      </div>`,
     layoutType: "FEATURE",
-    subtitle: "During ALL Sunday Services",
+    subtitle: "Durante TODOS los Servicios Dominicales",
     callsToAction: "",
     coverImage:
       "https://cloudfront.christfellowship.church/GetImage.ashx/GetImage.ashx?guid=950d64f7-96b7-47c4-9f98-5e4df3657a5e",
@@ -173,12 +199,12 @@ export const spanishFamiliesMappedChildren: ContentBlockData[] = [
   {
     id: "19039",
     type: "CONTENT_BLOCK",
-    name: "Speed up the Kids check-in process",
+    name: "Agiliza el proceso de registro de Niños",
     content:
-      "Plan your visit to let us know you're coming and to help speed up the check-in process. ",
+      "Planifica tu visita para avisarnos que vienes y ayudar a que el proceso de registro sea más rápido.",
     layoutType: "CARD",
     subtitle: "",
-    callsToAction: "Plan your first Visit^/#set-a-reminder",
+    callsToAction: "Planifica tu primera visita^/#set-a-reminder",
     coverImage: undefined,
     featureVideo: undefined,
     aspectRatio: "16by9",
@@ -202,12 +228,12 @@ export const spanishFamiliesMappedChildren: ContentBlockData[] = [
   {
     id: "19048",
     type: "CONTENT_BLOCK",
-    name: "The MIX (5th & 6th Grade)",
+    name: "The MIX (5.º y 6.º Grado)",
     content:
-      "5th and 6th grade is a time of transition—which is why it’s the perfect time to build a foundation of faith. The MIX meets every weekend at several campus locations.",
+      "Quinto y sexto grado son una etapa de transición, y por eso es el momento perfecto para construir una base sólida de fe. The MIX se reúne todos los fines de semana en el campus de Royal Palm en el servicio de las 5:00 P.M.",
     layoutType: "FULLSCREEN",
     subtitle: "",
-    callsToAction: "Times and Information ^#info",
+    callsToAction: "Horarios e información^#info",
     coverImage: undefined,
     featureVideo: undefined,
     aspectRatio: "16by9",
@@ -217,9 +243,9 @@ export const spanishFamiliesMappedChildren: ContentBlockData[] = [
   {
     id: "19046",
     type: "CONTENT_BLOCK",
-    name: "For Students",
+    name: "Para Estudiantes",
     content:
-      '<div style="color: #222222">If you\'re a middle or high school student, we\'d love to see you on <b>Wednesday nights at 6:30PM</b>. Our worship services, caring group leaders, and biblical teachings exist to help you know God personally as you make your faith your own. If you can\'t join us in person, we\'d love for you to join online.<div><br></div><div><b>CFStudents En Español meet on Thursdays.</b></div><div><br></div><div><b>Special Needs</b> </div><div>We offer families the opportunity to pair their student with a volunteer "Buddy" while they participate in the CFStudents environment.</div><div><a href="https://rock.gocf.org/page/2207" target="_blank" style="color: #0092BC; text-decoration: underline;">LEARN MORE HERE</a></div><div><br></div></div>',
+      '<div style="color: #222222">Si eres un estudiante de escuela media o secundaria, nos encantaría verte los <b>jueves a las 6:30 P.M.</b>. Nuestros servicios de adoración, líderes de grupos de apoyo y enseñanzas bíblicas existen para ayudarte a conocer a Dios personalmente mientras construyes tu propia fe. Si no puedes unirte en persona, nos encantaría que te unieras en línea.<div></div><div><br></div><div><b>Necesidades Especiales</b> </div><div>Ofrecemos a las familias la oportunidad de asignar a su estudiante un voluntario <b>“Buddy”</b> mientras participa en el ambiente de <i>CFStudents</i>.</div><div><a href="https://rock.gocf.org/page/2207" target="_blank" style="color: #0092BC; text-decoration: underline;">APRENDE MÁS AQUÍ</a></div><div><br></div></div>',
     layoutType: "FEATURE",
     subtitle: "",
     callsToAction: "",
@@ -233,12 +259,13 @@ export const spanishFamiliesMappedChildren: ContentBlockData[] = [
   {
     id: "19045",
     type: "CONTENT_BLOCK",
-    name: "Middle or High School Student",
+    name: "Estudiante de Escuela Intermedia o Secundaria",
     content:
-      "If you’re a middle or high school student, we’d love to see you on Wednesday nights at 6:30PM.",
+      "Si eres estudiante de escuela intermedia o secundaria, nos encantaría verte los jueves por la noche en el campus de Royal Palm Beach a las 6:30 p.m.",
     layoutType: "CARD",
     subtitle: "",
-    callsToAction: "Times and Info^/locations|Join Online^#youtube",
+    callsToAction:
+      "Horarios e información^/ministries/students|Únete en Línea^https://www.youtube.com/@cfstudentsfl",
     coverImage: undefined,
     featureVideo: undefined,
     aspectRatio: "16by9",
