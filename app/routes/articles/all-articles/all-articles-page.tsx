a93af4709365b117f3f419ff11efa3843cf46bdb
Leandro Alfonso <Leandroalfonso14@gmail.com>
2026-02-13 15:04:46 -0500

    Articles + Messages mobile fix

--- app/routes/articles/all-articles/all-articles-page.tsx
+++ app/routes/articles/all-articles/all-articles-page.tsx
@@ -3,7 +3,7 @@ import { AllArticles } from "./partials/all-articles.partial";
 
 export function AllArticlesPage() {
   return (
-    <div className="flex min-h-[100svh] max-h-[100svh] flex-col overflow-hidden md:max-h-none md:min-h-0 md:overflow-visible">
+    <div className="flex flex-col">
       <div className="flex-none">
         <DynamicHero
           customTitle="Articles"
@@ -11,9 +11,7 @@ export function AllArticlesPage() {
           ctas={[{ href: "/class-finder", title: "Take a Class" }]}
         />
       </div>
-      <div className="min-h-0 flex-1 overflow-y-auto md:overflow-visible">
-        <AllArticles />
-      </div>
+      <AllArticles />
     </div>
   );
 }
--- app/routes/messages/all-messages/messages-page.tsx
+++ app/routes/messages/all-messages/messages-page.tsx
@@ -4,7 +4,7 @@ import { AllMessages } from "./partials/all-messages.partial";
 
 export function MessagesPage() {
   return (
-    <div className="flex min-h-[100svh] max-h-[100svh] flex-col overflow-hidden md:max-h-none md:min-h-0 md:overflow-visible">
+    <div className="flex flex-col">
       <div className="flex-none">
         <DynamicHero
           imagePath="/assets/images/messages-hero-bg.jpg"
@@ -17,10 +17,8 @@ export function MessagesPage() {
           ]}
         />
       </div>
-      <div className="min-h-0 flex-1 overflow-y-auto md:overflow-visible">
-        <CurrentSeries />
-        <AllMessages />
-      </div>
+      <CurrentSeries />
+      <AllMessages />
     </div>
   );
 }
