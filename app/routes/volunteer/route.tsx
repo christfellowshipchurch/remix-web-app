fe1b05f15e197e3c37c919845c268c2f3fb4653a
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2026-04-16 15:32:47 -0400

    Removed Deprecated Sections/Logic
    Removed OnboardingProcess and FeaturedEvent sections. They are no longer used in new design

--- app/routes/volunteer/route.tsx
+++ app/routes/volunteer/route.tsx
@@ -3,32 +3,23 @@ import { VolunteerAtChurch } from "./partials/volunteer-church.partial";
 import { VolunteerWhere } from "./partials/volunteer-where.partial";
 import { VolunteerCommunity } from "./partials/volunteer-community.partial";
 import { VolunteerGlobe } from "./partials/volunteer-globe.partial";
-import { OnboardingProcess } from "./partials/volunteer-onboarding.partial";
 import { VolunteerStats } from "./partials/volunteer-stats.partial";
-import { VolunteerFeaturedEvent } from "./partials/volunteer-feature-event.partial";
-// import { VolunteerTestimonials } from "./partials/volunteer-testimonials.partial";
-import { useLoaderData } from "react-router";
-import { LoaderReturnType } from "./loader";
+import { VolunteerTestimonials } from "./partials/volunteer-testimonials.partial";
 
 export { loader } from "./loader";
 export { meta } from "./meta";
 
 function VolunteerPage() {
-  const { featuredEvent } = useLoaderData<LoaderReturnType>();
-
   return (
     <div>
       <VolunteerHero />
-
       <VolunteerWhere />
       <VolunteerAtChurch />
       <VolunteerCommunity />
       <VolunteerGlobe />
-      <OnboardingProcess />
       <VolunteerStats />
-
-      {featuredEvent && <VolunteerFeaturedEvent />}
-      {/* <VolunteerTestimonials /> */}
+      <VolunteerTestimonials />
+      {/* Add ready to make a difference component here */}
     </div>
   );
 }
