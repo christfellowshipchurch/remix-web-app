37451103d55c1e13983d9be3b222cb11af052138
Leandro Alfonso <Leandroalfonso14@gmail.com>
2026-05-05 11:15:59 -0400

    UI Fixes

--- app/routes/studies-and-resources/studies-single/partials/basic-content.partial.tsx
+++ app/routes/studies-and-resources/studies-single/partials/basic-content.partial.tsx
@@ -1,66 +1,88 @@
-import { Link } from "react-router-dom";
-import { IconName } from "~/primitives/button/types";
-import { Icon } from "~/primitives/icon/icon";
-import { Button } from "~/primitives/button/button.primitive";
-import HTMLRenderer from "~/primitives/html-renderer";
-import { StudyHitType } from "../../types";
-import { CurriculumItem } from "../components/curriculum-item.component";
+import { Link, useLocation } from 'react-router-dom';
+import { IconName } from '~/primitives/button/types';
+import { Icon } from '~/primitives/icon/icon';
+import { Button } from '~/primitives/button/button.primitive';
+import HTMLRenderer from '~/primitives/html-renderer';
+import { StudyHitType } from '../../types';
+import { CurriculumItem } from '../components/curriculum-item.component';
+import { Breadcrumbs } from '~/components';
 
 export function StudySingleBasicContent({ hit }: { hit: StudyHitType }) {
   const { title, description, audience, source, duration, format } = hit;
+  const location = useLocation();
+  const backToStudiesFinderUrl =
+    typeof location.state?.fromStudiesFinder === 'string'
+      ? location.state.fromStudiesFinder
+      : '/studies-and-resources';
 
   return (
-    <div className="w-full pb-12 lg:pb-16 xl:pb-20">
-      <div className="flex flex-col md:flex-row gap-12 lg:gap-20">
-        <div className="flex flex-col gap-4">
-          <h1 className="text-[40px] lg:text-[52px] font-extrabold leading-tight">
+    <div className='w-full pb-12 lg:pb-16 xl:pb-20'>
+      <div className='w-full flex flex-col md:flex-row md:justify-center gap-12 lg:gap-20'>
+        <div className='w-full max-w-[740px] flex flex-col gap-4'>
+          {/* Breadcrumbs */}
+          <div className='hidden md:flex gap-6 items-center mb-8'>
+            <Link
+              className='cursor-pointer text-text-secondary hover:text-ocean flex items-center gap-2'
+              to={backToStudiesFinderUrl}
+            >
+              <Icon name='arrowBack' className='size-6' />
+              <span className='hover:underline text-sm line-clamp-2 md:hidden'>
+                Back to Studies and Resources Finder
+              </span>
+            </Link>
+            <div className='hidden md:block'>
+              <Breadcrumbs />
+            </div>
+          </div>
+
+          <h1 className='text-[40px] lg:text-[52px] font-extrabold leading-none md:leading-tight'>
             {title}
           </h1>
-          <div className="flex flex-wrap gap-2">
-            <StudiesTagItem icon="bible" label={format} />
-            <StudiesTagItem icon="alarm" label={duration} />
-            <StudiesTagItem icon="user" label={audience} />
-            <StudiesTagItem icon="church" label={source} />
+          <div className='flex flex-wrap gap-2'>
+            <StudiesTagItem icon='bible' label={format} />
+            <StudiesTagItem icon='alarm' label={duration} />
+            <StudiesTagItem icon='user' label={audience} />
+            <StudiesTagItem icon='church' label={source} />
           </div>
 
           {/* Mobile Top Side */}
-          <div className="md:hidden">
+          <div className='md:hidden'>
             <RightSide title={title} source={source} />
           </div>
 
-          <div className="flex flex-col gap-4 mt-8 md:mt-12">
-            <h2 className="text-lg font-extrabold md:hidden">About</h2>
+          <div className='flex flex-col gap-4 mt-8 md:mt-12'>
+            <h2 className='text-lg font-extrabold md:hidden'>About</h2>
             <HTMLRenderer
               html={description}
-              className="text-text-primary md:text-lg md:font-medium "
+              className='text-text-primary md:text-lg md:font-medium '
             />
           </div>
 
           {/* Desktop Curriculum Section */}
-          <div className="hidden md:flex flex-col gap-5.5 md:mt-12 p-4 pb-8 rounded-2xl border border-[#ECEBEF] bg-gray">
-            <h3 className="text-lg font-semibold text-black leading-tight">
+          <div className='hidden md:flex flex-col gap-5.5 md:mt-12 p-4 pb-8 rounded-2xl border border-[#ECEBEF] bg-gray'>
+            <h3 className='text-lg font-semibold text-black leading-tight'>
               Curriculum
             </h3>
-            <div className="flex flex-col gap-4">
+            <div className='flex flex-col gap-4'>
               <CurriculumItem
-                title="Week 1: Getting Started"
-                subtitle="Release date: June 12, 2024"
+                title='Week 1: Getting Started'
+                subtitle='Release date: June 12, 2024'
                 items={[
                   {
-                    type: "Video",
-                    description: "Week 1: Getting Started",
-                    wistiaId: "wcs977y9ac",
+                    type: 'Video',
+                    description: 'Week 1: Getting Started',
+                    wistiaId: 'wcs977y9ac',
                   },
                 ]}
               />
               <CurriculumItem
-                title="Week 1: Getting Started"
-                subtitle="Release date: June 12, 2024"
+                title='Week 1: Getting Started'
+                subtitle='Release date: June 12, 2024'
                 items={[
                   {
-                    type: "Video",
-                    description: "Week 1: Getting Started",
-                    wistiaId: "wcs977y9ac",
+                    type: 'Video',
+                    description: 'Week 1: Getting Started',
+                    wistiaId: 'wcs977y9ac',
                   },
                 ]}
               />
@@ -69,7 +91,7 @@ export function StudySingleBasicContent({ hit }: { hit: StudyHitType }) {
         </div>
 
         {/* Desktop Right side */}
-        <div className="hidden md:block">
+        <div className='hidden md:block max-w-[324px] w-full'>
           <RightSide title={title} source={source} />
         </div>
       </div>
@@ -79,43 +101,43 @@ export function StudySingleBasicContent({ hit }: { hit: StudyHitType }) {
 
 const RightSide = ({ title, source }: { title: string; source: string }) => {
   return (
-    <div className="w-full md:max-w-[324px] flex flex-col mt-12 rounded-2xl overflow-hidden">
-      <div className="w-full flex gap-2.5 items-center px-6 py-8 bg-gray">
-        <div className="size-[82px] flex items-center justify-center bg-white p-2 rounded-[12px]">
+    <div className='w-full flex flex-col mt-12 rounded-2xl overflow-hidden'>
+      <div className='w-full flex gap-2.5 items-center px-6 py-8 bg-gray'>
+        <div className='size-[82px] flex items-center justify-center bg-white p-2 rounded-[12px]'>
           <img
-            src="/cf-logo.png" // TODO: Update to Source Image
+            src='/cf-logo.png' // TODO: Update to Source Image
             alt={title}
-            className="w-full h-full object-cover"
+            className='w-full h-full object-cover'
           />
         </div>
-        <div className="w-fit flex flex-col gap-0.5 text-sm font-semibold text-neutral-default">
+        <div className='w-fit flex flex-col gap-0.5 text-sm font-semibold text-neutral-default'>
           <p>Created by:</p>
-          <h3 className="font-bold text-text-primary">{source}</h3>
+          <h3 className='font-bold text-text-primary'>{source}</h3>
         </div>
       </div>
 
-      <div className="w-full flex flex-col gap-6 px-6 py-8 bg-dark-navy text-white">
-        <Button intent="secondaryWhite" size="md" className="w-full">
+      <div className='w-full flex flex-col gap-6 px-6 py-8 bg-dark-navy text-white'>
+        <Button intent='secondaryWhite' size='md' className='w-full'>
           Video Preview
         </Button>
-        <Button intent="secondaryWhite" size="md" className="w-full">
+        <Button intent='secondaryWhite' size='md' className='w-full'>
           Discussion Guide Preview
         </Button>
-        <Button intent="primary" size="md" className="w-full">
+        <Button intent='primary' size='md' className='w-full'>
           Start Study
         </Button>
 
-        <div className="flex flex-col gap-2">
-          <h3 className="text-lg font-extrabold leading-tight">
+        <div className='flex flex-col gap-2'>
+          <h3 className='text-lg font-extrabold leading-tight'>
             More Information
           </h3>
-          <p className="text-neutral-lighter leading-tight">
+          <p className='text-neutral-lighter leading-tight'>
             This curriculum is designed to help you learn more about the Bible
             and how to apply it to your life.
           </p>
           <Link
-            to={"mailto:groups@christfellowship.church"}
-            className="underline mt-1"
+            to={'mailto:groups@christfellowship.church'}
+            className='underline mt-1'
           >
             groups@christfellowship.church
           </Link>
@@ -133,8 +155,8 @@ export const StudiesTagItem = ({
   label: string;
 }) => {
   return (
-    <div className="flex items-center gap-2 bg-[#EBEBEB] w-fit rounded-sm text-xs font-semibold px-2 py-1">
-      {icon && <Icon name={icon} size={16} color="black" />}
+    <div className='flex items-center gap-2 bg-[#EBEBEB] w-fit rounded-sm text-xs font-semibold px-2 py-1'>
+      {icon && <Icon name={icon} size={16} color='black' />}
       <span>{label}</span>
     </div>
   );
