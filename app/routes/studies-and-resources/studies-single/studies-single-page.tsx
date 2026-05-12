f80df8d3f0f9c703b8702c9ce9bbd72622f9d459
Leandro Alfonso <Leandroalfonso14@gmail.com>
2026-05-12 10:19:44 -0400

    simplification

--- app/routes/studies-and-resources/studies-single/studies-single-page.tsx
+++ app/routes/studies-and-resources/studies-single/studies-single-page.tsx
@@ -3,7 +3,6 @@ import { LoaderReturnType } from './loader';
 
 import { StudySingleBasicContent } from './partials/basic-content.partial';
 import { Button } from '~/primitives/button/button.primitive';
-import { StudyHitType } from '../types';
 import { CurriculumItem } from './components/curriculum-item.component';
 
 const StudyNotFound = () => {
@@ -21,69 +20,59 @@ const StudyNotFound = () => {
   );
 };
 
-const StudySingleContent = ({ hit }: { hit: StudyHitType }) => {
-  if (!hit) {
-    return <StudyNotFound />;
-  }
-
-  return (
-    <section className='w-full flex flex-col items-center dark:bg-gray-900'>
-      {hit?.coverImage?.sources[0]?.uri && (
-        <img
-          src={hit.coverImage.sources[0].uri}
-          alt={hit.title}
-          className='w-full h-[250px] lg:h-[500px] object-cover overflow-hidden shrink-0'
-        />
-      )}
-
-      {/* Content */}
-      <div className='content-padding w-full flex flex-col items-center pt-8 md:pt-10'>
-        <div className='w-full flex flex-col items-center max-w-screen-content'>
-          <StudySingleBasicContent hit={hit} />
-        </div>
-      </div>
-
-      {/* Mobile Curriculum Section */}
-      <div className='flex md:hidden flex-col gap-5.5 md:mt-12 pt-8 pb-12 content-padding bg-gray w-full'>
-        <h3 className='text-lg font-semibold text-black leading-tight'>
-          Curriculum
-        </h3>
-        <div className='flex flex-col gap-4'>
-          <CurriculumItem
-            title='Week 1: Getting Started'
-            subtitle='Release date: June 12, 2024'
-            items={[
-              {
-                type: 'Video',
-                description: 'Week 1: Getting Started',
-                wistiaId: 'wcs977y9ac',
-              },
-            ]}
-          />
-          <CurriculumItem
-            title='Week 1: Getting Started'
-            subtitle='Release date: June 12, 2024'
-            items={[
-              {
-                type: 'Video',
-                description: 'Week 1: Getting Started',
-                wistiaId: 'wcs977y9ac',
-              },
-            ]}
-          />
-        </div>
-      </div>
-    </section>
-  );
-};
-
 export function StudiesSinglePage() {
   const { studyHit } = useLoaderData<LoaderReturnType>();
 
   return (
     <div className='w-full'>
       {studyHit ? (
-        <StudySingleContent key={studyHit.objectID} hit={studyHit} />
+        <section className='w-full flex flex-col items-center dark:bg-gray-900'>
+          {studyHit?.coverImage?.sources[0]?.uri && (
+            <img
+              src={studyHit.coverImage.sources[0].uri}
+              alt={studyHit.title}
+              className='w-full h-[250px] lg:h-[500px] object-cover overflow-hidden shrink-0'
+            />
+          )}
+
+          {/* Content */}
+          <div className='content-padding w-full flex flex-col items-center pt-8 md:pt-10'>
+            <div className='w-full flex flex-col items-center max-w-screen-content'>
+              <StudySingleBasicContent hit={studyHit} />
+            </div>
+          </div>
+
+          {/* Mobile Curriculum Section */}
+          <div className='flex md:hidden flex-col gap-5.5 md:mt-12 pt-8 pb-12 content-padding bg-gray w-full'>
+            <h3 className='text-lg font-semibold text-black leading-tight'>
+              Curriculum
+            </h3>
+            <div className='flex flex-col gap-4'>
+              <CurriculumItem
+                title='Week 1: Getting Started'
+                subtitle='Release date: June 12, 2024'
+                items={[
+                  {
+                    type: 'Video',
+                    description: 'Week 1: Getting Started',
+                    wistiaId: 'wcs977y9ac',
+                  },
+                ]}
+              />
+              <CurriculumItem
+                title='Week 1: Getting Started'
+                subtitle='Release date: June 12, 2024'
+                items={[
+                  {
+                    type: 'Video',
+                    description: 'Week 1: Getting Started',
+                    wistiaId: 'wcs977y9ac',
+                  },
+                ]}
+              />
+            </div>
+          </div>
+        </section>
       ) : (
         <StudyNotFound />
       )}
