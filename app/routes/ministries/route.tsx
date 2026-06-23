c86739bdebdc1cd3a20be1c6f4161ffd5860045c
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2026-06-23 16:50:41 -0400

    Remove All Ministries Page Hero

--- app/routes/ministries/route.tsx
+++ app/routes/ministries/route.tsx
@@ -1,4 +1,3 @@
-import { DynamicHero } from '~/components';
 import { AllMinistriesPartial } from './all-ministries/partials/all-ministries.partial';
 import { useLoaderData } from 'react-router-dom';
 import type { Ministry } from './loader';
@@ -9,13 +8,5 @@ export { meta } from './meta';
 export default function AllMinistriesPage() {
   const { ministries } = useLoaderData<{ ministries: Ministry[] }>();
 
-  return (
-    <div>
-      <DynamicHero
-        imagePath='/assets/images/ministries-hero.webp'
-        ctas={[{ href: '/volunteer', title: 'Volunteer' }]}
-      />
-      <AllMinistriesPartial ministries={ministries} />
-    </div>
-  );
+  return <AllMinistriesPartial ministries={ministries} />;
 }
