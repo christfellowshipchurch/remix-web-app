bbfae7b67ee13bde92baae2f1958c9d01977261e
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2026-07-28 09:40:32 -0400

    Fixed the kingdom builder links properly

--- app/routes/give/partials/what-bible-says.partial.tsx
+++ app/routes/give/partials/what-bible-says.partial.tsx
@@ -35,7 +35,7 @@ export const WhatBibleSaysAboutGiving = () => {
           <Card
             title='Offerings'
             description='GIVING BEYOND THE TITHE.'
-            content={`As Pastor Todd has shared, "We're never more like Jesus than when we serve and give." In Scripture, we're told that God so loved the world that He gave. Our offerings go beyond the obedience of the tithe; they are a reflection of a life marked by generosity. We believe generosity has a divine purpose connected to it, which is why we invite our church family to give beyond the tithe in one of these ways throughout the year. <br/><br/>Learn more about special offerings like <a class='text-ocean font-bold underline' target="_blank" href="https://www.impactoffering.church">Impact Offering</a> and <a class='text-ocean font-bold underline' href="/ministries/kingdom-builder">Kingdom Builders.</a>`}
+            content={`As Pastor Todd has shared, "We're never more like Jesus than when we serve and give." In Scripture, we're told that God so loved the world that He gave. Our offerings go beyond the obedience of the tithe; they are a reflection of a life marked by generosity. We believe generosity has a divine purpose connected to it, which is why we invite our church family to give beyond the tithe in one of these ways throughout the year. <br/><br/>Learn more about special offerings like <a class='text-ocean font-bold underline' target="_blank" href="https://www.impactoffering.church">Impact Offering</a> and <a class='text-ocean font-bold underline' href="/ministries/kingdom-builders">Kingdom Builders.</a>`}
           />
         </div>
 
